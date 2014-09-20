a557cacc2e5e3a65bc0efbe43373ab86ab6030d4
randomguyeatspotato <randomguyeatspotato@gmail.com>
2014-09-19 19:25:22 -0500

    tied up lose ends on structure merging
    allowed side and orientation to effect how they are merged

--- main.lua
+++ main.lua
@@ -18,14 +18,6 @@ function love.load()
 	for i=1,10 do
 		worldStructures[i] = Structure.create(Block.create(), world, i*35, i*35)
 	end
-
-	worldStructures[1]:merge(worldStructures[2], worldStructures[2].parts[1], 
-							 worldStructures[1].parts[1], "right")
-	table.remove(worldStructures, 2)
-	worldStructures[2]:merge(worldStructures[3], worldStructures[3].parts[1], 
-							 worldStructures[2].parts[1], "right")
-	table.remove(worldStructures, 3)
-	
 	debugmode = true
 end
 

4fc094746454cc01000b809f4d7d9b589def05ed
Jordan Christiansen <xordspar0@gmail.com>
2014-11-01 16:40:57 -0500

    Finished making side 1 the top of a part

--- selection.lua
+++ selection.lua
@@ -228,16 +228,16 @@ function Selection:draw(globalOffsetX, globalOffsetY)
 		local offsetX, offsetY -- move the cursor the side that we are selecting
 		if self.index == 1 then
 			offsetX, offsetY =
-				computeAbsCoords(10, 0, self.structure.body:getAngle())
+				computeAbsCoords(0, -10, self.structure.body:getAngle())
 		elseif self.index == 2 then
 			offsetX, offsetY =
-				computeAbsCoords(0, 10, self.structure.body:getAngle())
+				computeAbsCoords(10, 0, self.structure.body:getAngle())
 		elseif self.index == 3 then
 			offsetX, offsetY =
-				computeAbsCoords(-10, 0, self.structure.body:getAngle())
+				computeAbsCoords(0, 10, self.structure.body:getAngle())
 		elseif self.index == 4 then
 			offsetX, offsetY =
-				computeAbsCoords(0, -10, self.structure.body:getAngle())
+				computeAbsCoords(-10, 0, self.structure.body:getAngle())
 		end
 		love.graphics.draw(
 			self.image,

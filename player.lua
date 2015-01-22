synthein/synthein

commit 4f4371bf2ffab6381391ed55ac4ac26db45352fc

Reorganized main.lua

player.lua
@@ -1,6 +1,6 @@
 local Selection = require("selection")
 
-Player = {}
+local Player = {}
 Player.__index = Player
 
 function Player.create(type, structure)

selection.lua
@@ -1,6 +1,6 @@
-require("util")
+local Util = require("util")
 
-Selection = {}
+local Selection = {}
 Selection.__index = Selection
 
 function Selection.enable(structureList, ship, anchor)
@@ -171,15 +171,15 @@ function Selection:draw(globalOffsetX, globalOffsetY)
 		local y = self.structureList[self.index].body:getY()
 		love.graphics.draw(
 			self.image,
-			love.graphics.getWidth()/2 - globalOffsetX + x,
-			love.graphics.getHeight()/2 - globalOffsetY + y,
+			SCREEN_WIDTH/2 - globalOffsetX + x,
+			SCREEN_HEIGHT/2 - globalOffsetY + y,
 			0, 1, 1, self.width/2, self.width/2)
 	elseif self.mode == 2 then
 		local x, y = self.annexee:getAbsPartCoords(self.index)
 		love.graphics.draw(
 			self.image,
-			love.graphics.getWidth()/2 - globalOffsetX + x,
-			love.graphics.getHeight()/2 - globalOffsetY + y,
+			SCREEN_WIDTH/2 - globalOffsetX + x,
+			SCREEN_HEIGHT/2 - globalOffsetY + y,
 			0, 1, 1, self.width/2, self.width/2)
 	elseif self.mode == 3 then
 		local x, y = self.annexee:getAbsPartCoords(self.annexeePartIndex)
@@ -199,8 +199,8 @@ function Selection:draw(globalOffsetX, globalOffsetY)
 		end
 		love.graphics.draw(
 			self.image,
-			love.graphics.getWidth()/2 - globalOffsetX + x + offsetX,
-			love.graphics.getHeight()/2 - globalOffsetY + y + offsetY,
+			SCREEN_WIDTH/2 - globalOffsetX + x + offsetX,
+			SCREEN_HEIGHT/2 - globalOffsetY + y + offsetY,
 			0, 1, 1, self.width/2, self.width/2
 		)
 	elseif self.mode == 4 then
@@ -217,15 +217,15 @@ function Selection:draw(globalOffsetX, globalOffsetY)
 		end
 		love.graphics.draw(
 			self.image,
-			love.graphics.getWidth()/2 - globalOffsetX + x,
-			love.graphics.getHeight()/2 - globalOffsetY + y,
+			SCREEN_WIDTH/2 - globalOffsetX + x,
+			SCREEN_HEIGHT/2 - globalOffsetY + y,
 			0, 1, 1, self.width/2, self.width/2)
 	elseif self.mode == 5 then
 		local x, y = self.structure:getAbsPartCoords(self.index)
 		love.graphics.draw(
 			self.image,
-			love.graphics.getWidth()/2 - globalOffsetX + x,
-			love.graphics.getHeight()/2 - globalOffsetY + y,
+			SCREEN_WIDTH/2 - globalOffsetX + x,
+			SCREEN_HEIGHT/2 - globalOffsetY + y,
 			0, 1, 1, self.width/2, self.width/2)
 	elseif self.mode == 6 then
 		local x, y = self.structure:getAbsPartCoords(self.structurePartIndex)
@@ -245,8 +245,8 @@ function Selection:draw(globalOffsetX, globalOffsetY)
 		end
 		love.graphics.draw(
 			self.image,
-			love.graphics.getWidth()/2 - globalOffsetX + x + offsetX,
-			love.graphics.getHeight()/2 - globalOffsetY + y + offsetY,
+			SCREEN_WIDTH/2 - globalOffsetX + x + offsetX,
+			SCREEN_HEIGHT/2 - globalOffsetY + y + offsetY,
 			0, 1, 1, self.width/2, self.width/2
 		)
 	end

debugTools.lua
@@ -1,0 +1,33 @@
+local Block = require("block")
+local Engine = require("engine")
+local Gun = require("gun")
+local Structure = require("structure")
+
+local Debug = {}
+
+function Debug.keyboard(key, globalOffsetX, globalOffsetY)
+	-- Spawn a block
+	if key == "u" then
+		table.insert(worldStructures,
+		Structure.create(Block.create(), world,
+		globalOffsetX + 50, globalOffsetY - 100))
+	end
+	-- Spawn an engine
+	if key == "i" then
+		table.insert(worldStructures,
+		Structure.create(Engine.create(), world,
+		globalOffsetX + 112, globalOffsetY))
+	end
+	-- Spawn a gun
+	if key == "o" then
+		table.insert(worldStructures,
+		Structure.create(Gun.create(), world,
+		globalOffsetX + 50, globalOffsetY + 100))
+	end
+end
+
+function Debug.mouse(button)
+	print("not yet implemented")
+end
+
+return Debug

engine.lua
@@ -32,8 +32,8 @@ function Engine:draw(x, y, angle, globalOffsetX, globalOffsetY)
 
 	love.graphics.draw(
 		image,
-		love.graphics.getWidth()/2 - globalOffsetX + x,
-		love.graphics.getHeight()/2 - globalOffsetY + y,
+		SCREEN_WIDTH/2 - globalOffsetX + x,
+		SCREEN_HEIGHT/2 - globalOffsetY + y,
 		angle, 1, 1, self.width/2, self.height/2)
 end
 

initWorld.lua
@@ -1,0 +1,31 @@
+local Anchor = require("anchor")
+local Block = require("block")
+local Player = require("player")
+local PlayerBlock = require("playerBlock")
+local Structure = require("structure")
+
+local InitWorld = {}
+
+function InitWorld.init()
+	love.physics.setMeter(20) -- there are 20 pixels per meter
+	local world = love.physics.newWorld()
+
+	local worldStructures = {}
+	for i=1,10 do
+		worldStructures[i] = Structure.create(Block.create(), world, i*35, i*35)
+	end
+
+	-- Create the anchor.
+	local anchor = Structure.create(Anchor.create(), world, -10, -10)
+	anchor:addPart(Anchor.create(), 1, 0, 1)
+	anchor:addPart(Anchor.create(), 0, 1, 1)
+	anchor:addPart(Anchor.create(), 1, 1, 1)
+
+	-- Create the player.
+	local playerShip = Structure.create(PlayerBlock.create(), world, 0, -100)
+	local player1 = Player.create("player1", playerShip)
+
+	return world, worldStructures, anchor, player1, playerShip
+end
+
+return InitWorld

main.lua
@@ -1,44 +1,33 @@
-local Anchor = require("anchor")
-local Block = require("block")
-local Engine = require("engine")
-local Gun = require("gun")
+local Debug = require("debugTools")
+local InitWorld = require("initWorld")
 local Player = require("player")
-local PlayerBlock = require("playerBlock")
 local Structure = require("structure")
 
+local globalOffsetX
+local globalOffsetY
+
 function love.load()
-	love.physics.setMeter(20) -- there are 20 pixels per meter
-	world = love.physics.newWorld()
-
+	SCREEN_WIDTH = love.graphics.getWidth()
+	SCREEN_HEIGHT = love.graphics.getHeight()
 	compass = love.graphics.newImage("res/images/compass.png")
-
-	-- Create the player.
-	playerShip = Structure.create(PlayerBlock.create(), world, 0, -100)
-	player1 = Player.create("player1", playerShip)
-
-	-- Create the anchor.
-	anchor = Structure.create(Anchor.create(), world, -10, -10)
-	anchor:addPart(Anchor.create(), 1, 0, 1)
-	anchor:addPart(Anchor.create(), 0, 1, 1)
-	anchor:addPart(Anchor.create(), 1, 1, 1)
-
-	worldStructures = {}
-	for i=1,10 do
-		worldStructures[i] = Structure.create(Block.create(), world, i*35, i*35)
-	end
-
 	debugmode = true
+
+	world, worldStructures, anchor, player1, playerShip = InitWorld.init()
 end
 
 function love.update(dt)
 	world:update(dt)
-	globalOffsetX = player1.ship.body:getX()
-	globalOffsetY = player1.ship.body:getY()
 	playerShip:update()
 	player1:handleInput()
+
+	globalOffsetX = player1.ship.body:getX()
+	globalOffsetY = player1.ship.body:getY()
 end
 
 function love.draw()
+	SCREEN_WIDTH = love.graphics.getWidth()
+	SCREEN_HEIGHT = love.graphics.getHeight()
+
 	anchor:draw(globalOffsetX, globalOffsetY)
 	for i, structure in ipairs(worldStructures) do
 		structure:draw(globalOffsetX, globalOffsetY)
@@ -46,8 +35,8 @@ function love.draw()
 	player1:draw(globalOffsetX, globalOffsetY)
 	love.graphics.draw(
 		compass,
-		love.graphics.getWidth()-60,
-	    love.graphics.getHeight()-60,
+		SCREEN_WIDTH-60,
+	    SCREEN_HEIGHT-60,
 		math.atan2(playerShip.body:getY(),
 		playerShip.body:getX())-math.pi/2,
 		1, 1, 25, 25)
@@ -55,8 +44,8 @@ function love.draw()
 	if quitting then
 		love.graphics.print(
 			"Do you want to quit?",
-		    love.graphics.getWidth()/2-64,
-			love.graphics.getHeight()/2-30)
+		    SCREEN_WIDTH/2-64,
+			SCREEN_HEIGHT/2-30)
 	end
 
 	--------------------
@@ -76,32 +65,17 @@ function love.draw()
 	--------------------
 end
 
+function love.mousepressed(x, y, button)
+	if debugmode == true then
+		Debug.mouse(x, y, button)
+	end
+end
+
 function love.keypressed(key)
 	if key == "f11" then love.window.setFullscreen(not love.window.getFullscreen(), "desktop") end
 	if key == "f12" then debugmode = not debugmode end
 
-	--------------------
-	-- Debug Commands --
-	--------------------
 	if debugmode == true then
-		-- Spawn a block
-		if key == "u" then
-			table.insert(worldStructures,
-				Structure.create(Block.create(), world,
-				globalOffsetX + 50, globalOffsetY - 100))
-		end
-		-- Spawn an engine
-		if key == "i" then
-			table.insert(worldStructures,
-				Structure.create(Engine.create(), world,
-				globalOffsetX + 112, globalOffsetY))
-		end
-		-- Spawn a gun
-		if key == "o" then
-			table.insert(worldStructures,
-				Structure.create(Gun.create(), world,
-				globalOffsetX + 50, globalOffsetY + 100))
-		end
+		Debug.keyboard(key, globalOffsetX, globalOffsetY)
 	end
-	--------------------
 end

part.lua
@@ -20,8 +20,8 @@ end
 function Part:draw(x, y, angle, globalOffsetX, globalOffsetY)
 	love.graphics.draw(
 		self.image,
-		love.graphics.getWidth()/2 - globalOffsetX + x,
-		love.graphics.getHeight()/2 - globalOffsetY + y,
+		SCREEN_WIDTH/2 - globalOffsetX + x,
+		SCREEN_HEIGHT/2 - globalOffsetY + y,
 		angle, 1, 1, self.width/2, self.height/2)
 end
 

playerBlock.lua
@@ -17,8 +17,8 @@ function PlayerBlock.create()
 end
 
 function PlayerBlock:draw(x, y, angle)
-	love.graphics.draw(self.image, love.graphics.getWidth()/2,
-	                   love.graphics.getHeight()/2, angle,
+	love.graphics.draw(self.image, SCREEN_WIDTH/2,
+	                   SCREEN_HEIGHT/2, angle,
 					   1, 1, self.width/2, self.height/2)
 end
 

commit a3d236a65b14c50e85eb6f87407cd5b16f5c4650

Fixed annoying bugs introduced in 4f4371b

player.lua
@@ -134,7 +134,7 @@ function Player:handleInput()
 end
 
 function Player:draw(globalOffsetX, globalOffsetY)
-	self.ship:draw()
+	self.ship:draw(globalOffsetX, globalOffsetY)
 	if self.selection then
 		self.selection:draw(globalOffsetX, globalOffsetY)
 	end

selection.lua
@@ -186,16 +186,16 @@ function Selection:draw(globalOffsetX, globalOffsetY)
 		local offsetX, offsetY -- move the cursor the side that we are selecting
 		if self.index == 1 then
 			offsetX, offsetY =
-				computeAbsCoords(0, -10, self.annexee.body:getAngle())
+				Util.computeAbsCoords(0, -10, self.annexee.body:getAngle())
 		elseif self.index == 2 then
 			offsetX, offsetY =
-				computeAbsCoords(10, 0, self.annexee.body:getAngle())
+				Util.computeAbsCoords(10, 0, self.annexee.body:getAngle())
 		elseif self.index == 3 then
 			offsetX, offsetY =
-				computeAbsCoords(0, 10, self.annexee.body:getAngle())
+				Util.computeAbsCoords(0, 10, self.annexee.body:getAngle())
 		elseif self.index == 4 then
 			offsetX, offsetY =
-				computeAbsCoords(-10, 0, self.annexee.body:getAngle())
+				Util.computeAbsCoords(-10, 0, self.annexee.body:getAngle())
 		end
 		love.graphics.draw(
 			self.image,
@@ -232,16 +232,16 @@ function Selection:draw(globalOffsetX, globalOffsetY)
 		local offsetX, offsetY -- move the cursor the side that we are selecting
 		if self.index == 1 then
 			offsetX, offsetY =
-				computeAbsCoords(0, -10, self.structure.body:getAngle())
+				Util.computeAbsCoords(0, -10, self.structure.body:getAngle())
 		elseif self.index == 2 then
 			offsetX, offsetY =
-				computeAbsCoords(10, 0, self.structure.body:getAngle())
+				Util.computeAbsCoords(10, 0, self.structure.body:getAngle())
 		elseif self.index == 3 then
 			offsetX, offsetY =
-				computeAbsCoords(0, 10, self.structure.body:getAngle())
+				Util.computeAbsCoords(0, 10, self.structure.body:getAngle())
 		elseif self.index == 4 then
 			offsetX, offsetY =
-				computeAbsCoords(-10, 0, self.structure.body:getAngle())
+				Util.computeAbsCoords(-10, 0, self.structure.body:getAngle())
 		end
 		love.graphics.draw(
 			self.image,
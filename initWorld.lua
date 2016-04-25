6114a24acce4ff7374aaf040b94c7eee787033fc
randomguyeatspotato <randomguyeatspotato@gmail.com>
2016-04-25 01:54:17 -0500

    Added AI with follower programming (closes #9)

--- initWorld.lua
+++ initWorld.lua
@@ -1,6 +1,7 @@
 local Anchor = require("anchor")
 local Block = require("block")
 local ControlBlock = require("controlBlock")
+local AIBlock = require("aiBlock")
 local Structure = require("structure")
 
 local InitWorld = {}
@@ -11,7 +12,8 @@ function InitWorld.init(physics)
 	for i=1,10 do
 		worldStructures[i] = Structure.create(Block.create(), physics, i*35, i*35)
 	end
-
+	local aiShips = {}
+	aiShips[1] = Structure.create(AIBlock.create(), physics, -35, -35)
 	-- Create the anchor.
 	local anchor = Structure.create(Anchor.create(), physics, -10, -10)
 	anchor:addPart(Anchor.create(), 1, 0, 1)
@@ -21,7 +23,7 @@ function InitWorld.init(physics)
 	-- Create the player.
 	local playerShip = Structure.create(ControlBlock.create(), physics, 0, -100)
 
-	return worldStructures, anchor, playerShip, physics
+	return worldStructures, anchor, playerShip, aiShips, physics
 end
 
 return InitWorld

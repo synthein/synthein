d816472142250de7add0c753bec8c026234fac08
randomguyeatspotato <randomguyeatspotato@gmail.com>
2016-04-26 14:39:32 -0500

    Added loadable structures (closes #29) *Note AI
    Note: Added basic team data to AI will be edited for different commit.

--- ai.lua
+++ ai.lua
@@ -3,12 +3,12 @@ local Util = require("util")
 local AI = {}
 AI.__index = AI
 
-function AI.create(structure)
+function AI.create(structure, team)
 	local self = {}
 	setmetatable(self, AI)
 
 	self.ship = structure
-
+	self. team = team
 
 	return self
 end
@@ -19,16 +19,30 @@ function AI:update(dt, playerShip)
 	local playerX, playerY = playerShip:getAbsPartCoords(1)
 	local angle = Util.vectorAngle(playerX - aiX, playerY - aiY)
 	angle = aiAngle - angle - math.pi/2
-	if angle > math.pi then angle = angle - 2 * math.pi end
-	if angle  < -math.pi/15 then
-		table.insert(orders, "right")
-	elseif angle > math.pi/15 then
-		table.insert(orders, "left")
-	else 
-		table.insert(orders, "shoot")
-	end
-	if Util.vectorMagnitude(playerX - aiX, playerY - aiY) > 10 * 20 then
-		table.insert(orders, "forward")
+	if self.team == 1 then
+		if angle > math.pi then angle = angle - 2 * math.pi end
+		if angle  < -math.pi/10 then
+			table.insert(orders, "right")
+		elseif angle > math.pi/10 then
+			table.insert(orders, "left")
+		end
+		if Util.vectorMagnitude(playerX - aiX, playerY - aiY) > 10 * 20 then
+			table.insert(orders, "forward")
+		end
+	elseif self.team == 2 then
+		if angle > math.pi then angle = angle - 2 * math.pi end
+		if angle  < -math.pi/20 then
+			table.insert(orders, "right")
+		elseif angle > math.pi/20 then
+			table.insert(orders, "left")
+		else 
+			table.insert(orders, "shoot")
+		end
+		if Util.vectorMagnitude(playerX - aiX, playerY - aiY) > 15 * 20 then
+			table.insert(orders, "forward")
+		elseif Util.vectorMagnitude(playerX - aiX, playerY - aiY) < 10 * 20 then
+			table.insert(orders, "backward")
+		end
 	end
 	self.ship:command(orders)
 	self.ship:update(dt)

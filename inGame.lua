7d2cf1eb2ca49d42dbb497a3261dfebbf9a2214f
randomguyeatspotato <randomguyeatspotato@gmail.com>
2016-05-14 17:56:22 -0500

    Added pausing using the "p" key (closes #32)

--- inGame.lua
+++ inGame.lua
@@ -6,21 +6,25 @@ local World = require("world")
 local InGame = {}
 
 local world
+local paused = false
 
 function InGame.setWorld(setworld)
 	world = setworld
 end
 
 function InGame.update(dt)
-	physics:update(dt)
+	if paused then
+	else
+		physics:update(dt)
 
-	globalOffsetX = player1.ship.body:getX()
-	globalOffsetY = player1.ship.body:getY()
-	mouseWorldX = love.mouse.getX() - SCREEN_WIDTH/2 + globalOffsetX
-	mouseWorldY = love.mouse.getY() - SCREEN_HEIGHT/2 + globalOffsetY
+		globalOffsetX = player1.ship.body:getX()
+		globalOffsetY = player1.ship.body:getY()
+		mouseWorldX = love.mouse.getX() - SCREEN_WIDTH/2 + globalOffsetX
+		mouseWorldY = love.mouse.getY() - SCREEN_HEIGHT/2 + globalOffsetY
 
-	world:update(dt)
-	player1:handleInput(globalOffsetX, globalOffsetY)
+		world:update(dt)
+		player1:handleInput(globalOffsetX, globalOffsetY)
+	end
 	return InGame
 end
 
@@ -37,7 +41,12 @@ function InGame.draw()
 		SCREEN_HEIGHT-60,
 		math.atan2(globalOffsetY, globalOffsetX)-math.pi/2,
 		1, 1, 25, 25)
-
+	if paused then
+		love.graphics.print(
+			"Paused",
+			SCREEN_WIDTH/2-24,
+			SCREEN_HEIGHT/2-30)
+	end
 	if quitting then
 		love.graphics.print(
 			"Do you want to quit?",
@@ -48,6 +57,9 @@ function InGame.draw()
 end
 
 function InGame.keypressed(key)
+	if key == "p" then
+		paused = not paused
+	end
 	return InGame
 end
 

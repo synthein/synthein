4da8dd72528fe1ce1dbdc9fb3d7b41434d397aa7
randomguyeatspotato <randomguyeatspotato@gmail.com>
2016-09-01 17:39:33 -0500

    Moved translations into camera function.

--- camera.lua
+++ camera.lua
@@ -8,28 +8,31 @@ function Camera.create()
 	return self
 end
 
-function Camera.getX()
+function Camera:getX()
 	return self.x
 end
 
-function Camera.getY()
+function Camera:getY()
 	return self.y
 end
 
-function Camera.getPosition()
+function Camera:getPosition()
 	return self.x, self.y
 end
 
-function Camera.setX(newX)
+function Camera:setX(newX)
 	self.x = newX
 end
 
-function Camera.setY(newY)
+function Camera:setY(newY)
 	self.y = newY
 end
 
 function Camera:draw(image, x, y, angle, sx, sy, ox, oy)
-	love.graphics.draw(image, x, -y, -angle, sx, sy, ox, oy)
+	love.graphics.draw(image,
+					    (x - self.x) + SCREEN_WIDTH/2,
+					   -(y - self.y) + SCREEN_HEIGHT/2,
+					   -angle, sx, sy, ox, oy)
 end
 
 return Camera
--- screen.lua
+++ screen.lua
@@ -3,23 +3,30 @@ local Camera = require("camera")
 local Screen = {}
 Screen.__index = Screen
 
-Screen.camera = Camera.create()
+function Screen.createCameras()
+	Screen.camera = Camera.create()
+	Screen.camera:setX(0)
+	Screen.camera:setY(0)
+end
 
-function Screen:setCameras(numberOfCameras)
+function Screen.setCameras(numberOfCameras)
 	Screen.cameras = numberOfCameras
 end
 
-function Screen:setCamera(currentCamera)
+function Screen.setCamera(currentCamera)
 	Screen.camera = currentCamera
 end
 
 function Screen.getCursorCoords(X, Y)
-	cursorCoordX =   X - SCREEN_WIDTH /2  + Screen.camera.getX()
-	cursorCoordY = -(Y - SCREEN_HEIGHT/2) + Screen.camera.getY()
+	cursorCoordX =   X - SCREEN_WIDTH /2  + Screen.camera:getX()
+	cursorCoordY = -(Y - SCREEN_HEIGHT/2) + Screen.camera:getY()
 	return cursorCoordX, cursorCoordY
 end
-function Screen.draw(image, x,y, angle, sx, sy, ox, oy)
-	Screen.camera:draw(image, x,y, angle, sx, sy, ox, oy)
+function Screen.draw(image, x, y , angle, sx, sy, ox, oy)
+	Screen.camera:draw(image,
+					   x,
+					   y,
+					   angle, sx, sy, ox, oy)
 end
 
 return Screen

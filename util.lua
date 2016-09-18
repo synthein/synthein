33e414ea75a9fae914ac32c7e960fbf57de6b9fa
Drake <randomguyeatspotato@gmail.com>
2016-09-18 18:26:39 -0500

    Layed ground work for save files.

--- util.lua
+++ util.lua
@@ -58,4 +58,40 @@ function Util.min(a,b)
 	end
 end
 
+function Util.packLocation(input)
+	location = {}
+	if input.body then
+		location[1] = input.body:getX()
+		location[2] = input.body:getY()
+		location[3] = input.body:getAngle()
+		location[4], location[5] = input.body:getLinearVelocity()
+		location[6] = input.body:getAngularVelocity()
+	else
+		location = input
+	end
+	locationString = "("
+	for i in ipairs(location) do
+		if not (i == 1) then locationString = locationString .. ","	end
+		locationString = locationString .. tostring(location[i])
+	end
+	locationString = locationString .. ")"
+	return locationString
+end
+
+function Util.unpackLocation()
+end
+
+function Util.packData(data)
+	dataString = "["
+	for i in ipairs(data) do
+		if not (i == 1) then dataString = dataString .. ","	end
+		dataString = dataString .. tostring(data[i])
+	end
+	dataString = dataString .. "]"
+	return dataString
+end
+
+function Util.unpackLocation()
+end
+
 return Util

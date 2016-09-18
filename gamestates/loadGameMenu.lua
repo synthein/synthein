3d2486601542c47d37c9878c840f36f1d5148523
randomguyeatspotato <randomguyeatspotato@gmail.com>
2016-09-17 22:52:26 -0500

    Fixed crash from load button.

--- gamestates/loadGameMenu.lua
+++ gamestates/loadGameMenu.lua
@@ -3,21 +3,9 @@ local GameState = require("gamestates/gameState")
 local LoadGameMenu = {}
 setmetatable(LoadGameMenu, GameState)
 
--- Print debug info.
-function LoadGameMenu.draw()
-end
-
-function LoadGameMenu.update(mouseWorldX, mouseWorldY)
-end
-
 function LoadGameMenu.keypressed(key)
+	
 	return InGame
-end
-
-function LoadGameMenu.mousepressed(mouseX, mouseY, button, mouseWorldX, mouseWorldY)
-end
-
-function LoadGameMenu.mousereleased(x, y, button, mouseWorldX, mouseWorldY)
 end
 
 return LoadGameMenu

40eae5c444cd048e1e5915ffa81fc40285029252
Jordan Christiansen <xordspar0@gmail.com>
2016-12-11 21:16:32 -0600

    Added initial support for gamepads

--- player.lua
+++ player.lua
@@ -6,27 +6,13 @@ local Screen = require("screen")
 local Player = {}
 Player.__index = Player
 
-function Player.create(type, structure)
+function Player.create(controls, structure)
 	local self = {}
 	setmetatable(self, Player)
 
 	self.ship = structure
 
-	if type == "player1" then
-		self.forward = "w"
-		self.back = "s"
-		self.left = "a"
-		self.right = "d"
-		self.strafeLeft = "q"
-		self.strafeRight = "e"
-		self.selectPrevious = "v"
-		self.selectNext = "c"
-		self.removePart = "x"
-		self.confirm = "return"
-		self.cancel = "escape"
-		self.shoot = "space"
-	elseif type =="player2" then
-	end
+	self.controls = controls
 
 	self.selection = nil
 	self.cancelKeyDown = false
@@ -49,12 +35,12 @@ function Player:handleInput()
 	if not self.cancelKeyDown then
 		-- Open the pause menu.
 		if menuOpen then
-			if love.keyboard.isDown(self.cancel) then
+			if love.keyboard.isDown(self.controls.cancel) then
 				menuOpen = false
 				self.cancelKeyDown = true
 			end
 
-		elseif love.keyboard.isDown(self.cancel) then
+		elseif love.keyboard.isDown(self.controls.cancel) then
 			-- If selection mode is not enabled, quit the game when the cancel key
 			-- is pressed.
 			if not self.selection then
@@ -66,7 +52,7 @@ function Player:handleInput()
 			self.cancelKeyDown = true
 		end
 
-	elseif not love.keyboard.isDown(self.cancel) then
+	elseif not love.keyboard.isDown(self.controls.cancel) then
 		self.cancelKeyDown = false
 	end
 
@@ -75,26 +61,52 @@ function Player:handleInput()
 	-----------------------
 	local orders = {}
 
-	if love.keyboard.isDown(self.forward) then
-		table.insert(orders, "forward")
-	end
-	if love.keyboard.isDown(self.back) then
-		table.insert(orders, "back")
-	end
-	if love.keyboard.isDown(self.left) then
-		table.insert(orders, "left")
-	end
-	if love.keyboard.isDown(self.right) then
-		table.insert(orders, "right")
-	end
-	if love.keyboard.isDown(self.strafeLeft) then
-		table.insert(orders, "strafeLeft")
-	end
-	if love.keyboard.isDown(self.strafeRight) then
-		table.insert(orders, "strafeRight")
-	end
-	if love.keyboard.isDown(self.shoot) then
-		table.insert(orders, "shoot")
+	if self.controls.joystick then
+		local joystick = self.controls.joystick
+
+		if joystick:isGamepadDown(self.controls.forward) then
+			table.insert(orders, "forward")
+		end
+		if joystick:isGamepadDown(self.controls.back) then
+			table.insert(orders, "back")
+		end
+		if joystick:isGamepadDown(self.controls.left) then
+			table.insert(orders, "left")
+		end
+		if joystick:isGamepadDown(self.controls.right) then
+			table.insert(orders, "right")
+		end
+		if joystick:isGamepadDown(self.controls.strafeLeft) then
+			table.insert(orders, "strafeLeft")
+		end
+		if joystick:isGamepadDown(self.controls.strafeRight) then
+			table.insert(orders, "strafeRight")
+		end
+		if joystick:isGamepadDown(self.controls.shoot) then
+			table.insert(orders, "shoot")
+		end
+	else
+		if love.keyboard.isDown(self.controls.forward) then
+			table.insert(orders, "forward")
+		end
+		if love.keyboard.isDown(self.controls.back) then
+			table.insert(orders, "back")
+		end
+		if love.keyboard.isDown(self.controls.left) then
+			table.insert(orders, "left")
+		end
+		if love.keyboard.isDown(self.controls.right) then
+			table.insert(orders, "right")
+		end
+		if love.keyboard.isDown(self.controls.strafeLeft) then
+			table.insert(orders, "strafeLeft")
+		end
+		if love.keyboard.isDown(self.controls.strafeRight) then
+			table.insert(orders, "strafeRight")
+		end
+		if love.keyboard.isDown(self.controls.shoot) then
+			table.insert(orders, "shoot")
+		end
 	end
 
 	if self.ship then

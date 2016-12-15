a797df4b2cfd1d22a6c33548195d6a6666f3bc99
drake <randomguyeatspotato@gmail.com>
2016-12-14 19:02:41 -0600

    Moved drawing compass to the camera file.

--- camera.lua
+++ camera.lua
@@ -5,6 +5,7 @@ function Camera.create()
 	local self = {}
 	setmetatable(self, Camera)
 
+	self.compass = love.graphics.newImage("res/images/compass.png")
 	return self
 end
 
@@ -44,6 +45,15 @@ function Camera:draw(image, x, y, angle, sx, sy, ox, oy)
 					    (x - self.x) + SCREEN_WIDTH/2,
 					   -(y - self.y) + SCREEN_HEIGHT/2,
 					   -angle, sx, sy, ox, oy)
+end
+
+function Camera:drawExtras()
+	love.graphics.draw(
+			self.compass,
+			SCREEN_WIDTH-60,
+			SCREEN_HEIGHT-60,
+			math.atan2(self.x, self.y) + math.pi,
+			1, 1, 25, 25)
 end
 
 return Camera

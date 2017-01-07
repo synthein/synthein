75c7d37ac575007caa2740041393e1c02d3a39ad
randomguyeatspotato <randomguyeatspotato@gmail.com>
2017-01-07 14:47:22 -0600

    Changed controls to simplify player.lua

--- controls.lua
+++ controls.lua
@@ -1,28 +1,42 @@
 local Controls = {}
 
+local keyboard = love.keyboard
+local getJoysticks = love.joystick.getJoysticks
+
+function Controls.isDown(control)
+	return control[1].isDown(control[2])
+end
+
+function Controls.order(control)
+	return control[3]
+end
+
 Controls.defaults = {
 	keyboard = {
-		forward = "w",
-		back = "s",
-		left = "a",
-		right = "d",
-		strafeLeft = "q",
-		strafeRight = "e",
-		confirm = "return",
-		cancel = "escape",
-		shoot = "space"
+		ship = {
+			{keyboard, "w", "forward"},
+			{keyboard, "s", "back"},
+			{keyboard, "a", "left"},
+			{keyboard, "d", "right"},
+			{keyboard, "q", "strafeLeft"},
+			{keyboard, "e", "strafeRight"},
+			{keyboard, "space", "shoot"}
+		},
+		confirm		= {keyboard, "return"},
+		cancel		= {keyboard, "escape"}
 	},
 	gamepad = {
-		joystick = love.joystick.getJoysticks()[1],
-		forward = "dpup",
-		back = "dpdown",
-		left = "dpleft",
-		right = "dpright",
-		strafeLeft = "leftshoulder",
-		strafeRight = "rightshoulder",
-		confirm = "a",
-		cancel = "b",
-		shoot = "a"
+		ship = {
+			{getJoysticks()[1], "dpup", "forward"},
+			{getJoysticks()[1], "dpdown", "back"},
+			{getJoysticks()[1], "dpleft", "left"},
+			{getJoysticks()[1], "dpright", "right"},
+			{getJoysticks()[1], "leftshoulder", "strafeLeft"},
+			{getJoysticks()[1], "rightshoulder", "strafeRight"},
+			{getJoysticks()[1], "a", "shoot"}
+		},
+		confirm		= {getJoysticks()[1], "a"},
+		cancel		= {getJoysticks()[1], "b"}
 	}
 }
 

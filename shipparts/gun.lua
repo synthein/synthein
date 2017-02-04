e260b2a5ba34272f0e327d7e41426b17ea884081
drake <randomguyeatspotato@gmail.com>
2017-02-04 15:14:06 -0600

    Fixed guns.

--- shipparts/gun.lua
+++ shipparts/gun.lua
@@ -25,17 +25,23 @@ function Gun.create(world, x, y)
 	return self
 end
 
-function Gun:update(dt, partsInfo)
+function Gun:update(dt, partsInfo, location, locationSign, orientation)
 	if self.recharge then
 		self.rechargeStart = self.rechargeStart + dt
 		if self.rechargeStart > 0.5 then
 			self.recharge = false
 		end
 	end
-	if  partsInfo.guns and partsInfo.guns.shoot and not part.recharge then
+	if  partsInfo.guns and partsInfo.guns.shoot and not self.recharge then
+		local l = partsInfo.locationInfo[1]
+		local directionX = partsInfo.locationInfo[2][1]
+		local directionY = partsInfo.locationInfo[2][2]
+		a = {directionX, directionY}
+		local x = (location[1] * directionX - location[2] * directionY) * 20 + l[1]
+		local y = (location[1] * directionY + location[2] * directionX) * 20 + l[2]
 		self.recharge = true
 		self.rechargeStart = 0
-		local location = {self:getAbsPartCoords(i)}
+		local location = {x, y, l[3]}
 		return {"shots", location, self}
 	end
 end
--- shipparts/playerBlock.lua
+++ shipparts/playerBlock.lua
@@ -58,6 +58,18 @@ function PlayerBlock:update(dt, partsInfo, location, locationSign, orientation)
 			self.recharge = false
 		end
 	end
+	if  partsInfo.guns and partsInfo.guns.shoot and not self.recharge then
+		local l = partsInfo.locationInfo[1]
+		local directionX = partsInfo.locationInfo[2][1]
+		local directionY = partsInfo.locationInfo[2][2]
+		a = {directionX, directionY}
+		local x = (location[1] * directionX - location[2] * directionY) * 20 + l[1]
+		local y = (location[1] * directionY + location[2] * directionX) * 20 + l[2]
+		self.recharge = true
+		self.rechargeStart = 0
+		local location = {x, y, l[3]}
+		return {"shots", location, self}
+	end
 
 	
 	local engines = partsInfo.engines

d2c63df5d7b34cb5edeed85e6926097fbdb89669
Jordan Christiansen <xordspar0@gmail.com>
2017-03-19 15:35:54 -0500

    Added drone job to build windows packages

---
+++ scripts/package-windows.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ ! -f build/synthein.love ]; then
+	echo "Need to build the .love file first."
+	exit 1
+fi
+
+curl -O https://bitbucket.org/rude/love/downloads/love-0.10.2-win64.zip
+unzip love-0.10.2-win64.zip
+
+mkdir build/synthein-windows
+cat love-0.10.2-win64/love.exe build/synthein.love > build/synthein-windows/synthein.exe
+cp love-0.10.2-win64/*.dll love-0.10.2-win64/licence.txt build/synthein-windows
+
+cd build
+zip -r synthein-windows.zip synthein-windows/
+
+echo "Built $(find . -name synthein-windows.zip)."

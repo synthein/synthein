bd3c4dc17543e003c2c1d8bf2d256f831bcbecc9
drake <randomguyeatspotato@gmail.com>
2017-07-08 13:10:21 -0500

    Added physicsReferences.lua

---
+++ src/physicsReferences.lua
@@ -0,0 +1,52 @@
+local PhysicsReferences = {}
+
+--Contact Filter references
+--Fixture Categories
+
+local fixtures = {categories = {}, masks = {}, groups ={}}
+local categories = fixtures.categories
+local masks = fixtures.mask
+local groups = fixture.groups
+
+
+--If a collision occurs the fixture with the category that comes first will have
+--the collision function called on them. If the two fixtures of the same type
+--the collison function will be called on both.
+local catergoryOrder = {
+	"cameras", --players/what ships see
+	"visual", --animations
+	"shields",
+	"projectiles",
+	"general" --basic physics bodies
+}
+
+--Generate category values.
+local bit = 1
+for i,v in ipairs(catergoryOrder)
+	categories[v] = bit
+	bit = bit *2
+end
+
+--Anything included collides. It is required for both that are involed to
+--include the other.
+masks = {
+	camera = {"visual", "shields", "projectiles", "general"},
+	visual = {"cameras"},
+	shield = {"cameras", "projectiles"},
+	projectile = {"cameras", "shields", "projectiles", "general"},
+	general = {"cameras", "projectiles", "general"}
+}
+
+--Groups determine if they collide with themselfs
+local collision = {"missiles", "general"}
+local noCollision = {"cameras", "visual", "shields", "projectiles"}
+
+for i,v in ipairs(collision)
+	groups[v] = i
+end
+
+for i,v in ipairs(noCollision)
+	groups[v] = -i
+end
+
+return PhysicsReferences

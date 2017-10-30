5fed8fc44489af7ea13ce0373ec5e058201abb9d
Jordan Christiansen <Jordan.Christiansen@target.com>
2017-10-30 10:13:12 -0500

    Converted LoadGameMenu to use Menu object

--- src/gamestates/loadGameMenu.lua
+++ src/gamestates/loadGameMenu.lua
@@ -1,5 +1,6 @@
 local GameState = require("gamestates/gameState")
 local InGame = require("gamestates/inGame")
+local Menu = require("menu")
 local SceneParser = require("sceneParser")
 
 local InitWorld = require("initWorld")
@@ -8,51 +9,36 @@ local LoadGameMenu = {}
 setmetatable(LoadGameMenu, GameState)
 
 LoadGameMenu.font = love.graphics.newFont(18)
-local scrollPosition = 1
-local loadGameChoice
 local buttons = {}
+local files = love.filesystem.getDirectoryItems("saves")
+for i, fileName in pairs(files) do
+	buttonName = string.gsub(fileName, ".txt", "")
+	table.insert(buttons, buttonName)
+end
+LoadGameMenu.menu = Menu.create(love.graphics.getWidth()/2, 250, 5, buttons)
 
-function LoadGameMenu.mousepressed(x, y, mouseButton)
-	if mouseButton == 1 then
-		if x < (SCREEN_WIDTH - button_width)/2 or x > (SCREEN_WIDTH + button_width)/2 then
-			return MainMenu
-		end
-		local yRef = y - 75
-		local index = math.floor(yRef/75)
-		local remainder = yRef % 75
-
-		loadGameChoice = buttons[index]
+function LoadGameMenu.update(dt)
+	LoadGameMenu.menu.buttons = {}
+	for i, fileName in pairs(files) do
+		buttonName = string.gsub(fileName, ".txt", "")
+		table.insert(LoadGameMenu.menu.buttons, buttonName)
 	end
 end
 
-function LoadGameMenu.update(dt)
-	local files = love.filesystem.getDirectoryItems("saves")
+function LoadGameMenu.draw()
+	LoadGameMenu.menu:draw()
+end
 
-	buttons = {}
-	for i = scrollPosition, #files do
-		buttonName = string.gsub(files[i], ".txt", "")
-		table.insert(buttons, buttonName)
-	end
-
+function LoadGameMenu.mousepressed(x, y, mouseButton)
+	local loadGameChoice = LoadGameMenu.menu:pressed(x, y)
 	if loadGameChoice then
 		InitWorld.init("saves/" .. loadGameChoice, true)
 		table.insert(LoadGameMenu.stack, InGame)
 	end
 end
 
-function LoadGameMenu.draw()
-	previousFont = love.graphics.getFont()
-	love.graphics.setFont(LoadGameMenu.font)
-	button_width = 500
-	button_height = 50
-	text_height = 40
-	for i,button in ipairs(buttons) do
-		love.graphics.setColor(100, 100, 100)
-		love.graphics.rectangle("fill", (SCREEN_WIDTH - button_width)/2, 75 + 75 * i, button_width, button_height)
-		love.graphics.setColor(255, 255, 255)
-		love.graphics.print(buttons[i], (SCREEN_WIDTH - button_width)/2 + 10, 75 + 75 * i + button_height/2 - text_height/2, 0, 1, 1, 0, 0, 0, 0)
-	end
-	love.graphics.setFont(previousFont)
+function LoadGameMenu.resize(w, h)
+	MainMenu.menu:resize(w, h)
 end
 
 return LoadGameMenu

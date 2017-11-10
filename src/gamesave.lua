e648e930c1da9936902259d119373ec47ecb050d
Jordan Christiansen <xordspar0@gmail.com>
2017-11-09 21:45:31 -0600

    Added loading functionality to Savegame

--- src/gamesave.lua
+++ src/gamesave.lua
@@ -3,6 +3,19 @@ local SceneParser = require("sceneParser")
 local Gamesave = {}
 local saveDir = "saves/"
 
+-- Gamesave.load: Load a game from a file.
+-- Returns an iterator for the lines in a save file on success.
+-- Returns nil and a reason why on failure.
+function Gamesave.load(saveName)
+	local fileName = saveDir .. saveName .. ".txt"
+
+	if not love.filesystem.isFile(fileName) then
+		return nil, string.format("File %s does not exist", fileName)
+	end
+
+	return love.filesystem.lines(fileName)
+end
+
 -- Gamesave.save: Save the game to a file.
 -- Returns boolean success status and (optionally) a message explaining why
 -- there was a failure.

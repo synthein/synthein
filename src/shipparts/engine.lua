2e8fe346b36551791f00020134c8f072d8ef161b
randomguyeatspotato <randomguyeatspotato@gmail.com>
2017-11-24 14:56:15 -0600

    Cleaned up engine.lua

--- src/shipparts/engine.lua
+++ src/shipparts/engine.lua
@@ -1,83 +1,65 @@
+local StructureMath = require("structureMath")
 local Util = require("util")
 
 local Engine = {}
 Engine.__index = Engine
 setmetatable(Engine, Part)
 
-Engine.orientationVectors = {
-	{ 0,  1},
-	{-1,  0},
-	{ 0, -1},
-	{ 1,  0}}
-
-Engine.rotationTable = {
-	{ 1, 1}, --  x
-	{ 1, 2}, --  y
-	{-1, 1}, -- -x
-	{-1, 2}} -- -y
-
-Engine.directionTable = {
-	{-1, 2, 1, 1}, -- -y,  x	90
-	{-1, 1,-1, 2}, -- -x, -y	180
-	{ 1, 2,-1, 1}, --  y, -x	270
-	{ 1, 1, 1, 2}} --  x,  y	0
-
 function Engine.create(engineType, thrust, torque)
 	local self = {}
 	setmetatable(self, Engine)
 
-	self.isActive = false
 	self.thrust = thrust
 	if engineType == 1 then self.torque = torque end
 	self.engineType = engineType
+	-- Type
+	-- 1: Found in playerBlock and aiBlock and is an all direction engine
+	-- 2: Found in engineBlock is a single direction engine.
 
 	return self
 end
 
 function Engine:update(part, enginesInfo)
 	local body = part.fixture:getBody()
+	local x, y, orientation = unpack(part.location)
+	local active, fx, fy
 
-	if part.location and body and enginesInfo then
-		local angle = (part.location[3] - 1) * math.pi/2 + body:getAngle()
-		
-		local fx, fy
-		if self.engineType == 1 then
-			fx = enginesInfo[6]
-			fy = enginesInfo[5]
-			if enginesInfo[5] == 0 and enginesInfo[6] == 0
-					and enginesInfo[7] == 0 then
-				on = 0
-			else
-				on = 1
-			end
-		elseif self.engineType == 2 then
-			local r = Engine.rotationTable[part.location[3]]
-			local rotation = r[1] * Util.sign(part.location[r[2]])
-			on = enginesInfo[part.location[3]] + enginesInfo[7] * rotation
-			fx, fy = unpack(Engine.orientationVectors[part.location[3]])
-		end
-
-		if on > 0 then
-			self.isActive = true
-		else
-			self.isActive = false
-		end
-		
-		if self.isActive then
-			fx, fy = body:getWorldVector(fx, fy)
-			local x, y = unpack(part.location)
-			x, y = body:getWorldPoints(x, y)
-			body:applyForce(fx * self.thrust, fy * self.thrust, x, y)
-
-			if self.torque then
-				body:applyTorque(enginesInfo[7] * self.torque)
-			end
-		end
-	else
-		self.isActive = false
+	if not enginesInfo then
+		return false
 	end
 
-	return self.isActive
+	if self.engineType == 1 then
+		if enginesInfo[5] == 0 and 
+		   enginesInfo[6] == 0 and
+		   enginesInfo[7] == 0 then
+			active = false
+		else
+			-- Set local force direction
+			fx = enginesInfo[6]
+			fy = enginesInfo[5]
+			active = true
+		end
+	elseif self.engineType == 2 then
+		-- Determine if engine is pointing in a helpful direction.
+		local rotationTable = {x, y, -x, -y}
+		local rotation = Util.sign(rotationTable[orientation])
+		active = 0 < (enginesInfo[orientation] + enginesInfo[7] * rotation)
+		-- Determine local force direction
+		fx, fy = unpack(StructureMath.addUnitVector({0, 0}, orientation))
+	end
+	
+	if active then
+		-- Applying the forces and the torque
+		fx, fy = body:getWorldVector(fx, fy)
+		x, y = body:getWorldPoints(x, y)
+		body:applyForce(fx * self.thrust, fy * self.thrust, x, y)
+
+		if self.torque then
+			body:applyTorque(enginesInfo[7] * self.torque)
+		end
+	end
+
+	return active
 end
 
 return Engine

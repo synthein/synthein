37b7c8679f9f8444ad1f76fad9074a0e95626ed9
drake <randomguyeatspotato@gmail.com>
2017-11-24 10:38:07 -0600

    Prevented player block from shoting connected blocks.

--- src/shipparts/gun.lua
+++ src/shipparts/gun.lua
@@ -1,3 +1,5 @@
+local StructureMath = require("structureMath")
+
 local Gun = {}
 Gun.__index = Gun
 setmetatable(Gun, Part)
@@ -14,16 +16,27 @@ end
 
 function Gun:update(dt, shoot, part)
 	if self.recharge then
+		-- Recharge timer
 		self.rechargeTime = self.rechargeTime + dt
 		if self.rechargeTime > 0.5 then
+			self.rechargeTime = 0
 			self.recharge = false
 		end
 	else
 		if shoot then
-			self.recharge = true
-			self.rechargeTime = 0
-			local events = part.fixture:getBody():getUserData().events
-			table.insert(events.create, {"shots", {part:getWorldLocation()}, part})
+			local structure = part.fixture:getBody():getUserData()
+			-- Check if there is a part one block infront of the gun.
+			local l = part.location
+			local x, y = unpack(StructureMath.addUnitVector(l, l[3]))
+			local clear = not structure.gridTable:index(x, y)
+
+			if clear then
+				-- Start timer
+				self.recharge = true
+				-- Spawn Shot
+				local shot = {"shots", {part:getWorldLocation()}, part}
+				table.insert(structure.events.create, shot)
+			end
 		end
 	end
 end

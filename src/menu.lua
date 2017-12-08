synthein/synthein

commit 1a6d9ca9b1d945648af9e425ed57578a7d56ce8e

Hide all references to love.graphics outside of love.draw()
This makes it possible to run the game with the graphics modules turned
off. Parts of this are unpleasant hacks, such as menu fonts. I'm not
sure how to handle that just yet.

src/menu.lua
@@ -13,13 +13,13 @@ function Menu.create(x, y, size, buttons, camera)
 	self.textHeight = size * 8
 	self.x = x - self.buttonWidth/2
 	self.y = y
-	self.visibleHeight = love.graphics.getHeight() - self.y
+	self.visibleHeight = 0
 	self.buttons = buttons
 	self.scrollY = 0
 	self.scrollVelocity = 0
 	self.selectedButton = 0
-	self.font = love.graphics.newFont(size * 7)
 	self.camera = camera
+	if love.graphics then self.font = love.graphics.newFont(size * 7) end
 
 	return self
 end
@@ -78,6 +78,7 @@ function Menu:update(dt)
 end
 
 function Menu:draw()
+	self.visibleHeight = love.graphics.getHeight() - self.y
 	local x, y
 	if self.camera then
 		love.graphics.setScissor(self.camera:getScissor())

src/gamestates/inGame.lua
@@ -17,7 +17,7 @@ local second = 0
 
 local menuOpen = false
 local pauseMenu = {}
-pauseMenu.font = love.graphics.newFont(18)
+if love.graphics then pauseMenu.font = love.graphics.newFont(18) end
 pauseMenu.buttons = {"Save", "Main Menu", "Quit"}
 local typingSaveName = false
 local saveName = ""

src/gamestates/loadGameMenu.lua
@@ -12,7 +12,7 @@ for i, fileName in pairs(files) do
 	buttonName = string.gsub(fileName, ".txt", "")
 	table.insert(buttons, buttonName)
 end
-LoadGameMenu.menu = Menu.create(love.graphics.getWidth()/2, 250, 5, buttons)
+if love.graphics then LoadGameMenu.menu = Menu.create(love.graphics.getWidth()/2, 250, 5, buttons) end
 
 function LoadGameMenu.update(dt)
 	LoadGameMenu.menu.buttons = {}

src/gamestates/mainMenu.lua
@@ -6,10 +6,14 @@ local NewGameMenu = require("gamestates/newGameMenu")
 local MainMenu = {}
 setmetatable(MainMenu, GameState)
 
-MainMenu.font = love.graphics.newFont(36)
 local buttons = {NewGameMenu, LoadGameMenu}
 local buttonNames = {"New Game", "Load Game"}
-MainMenu.menu = Menu.create(love.graphics.getWidth()/2, 250, 5, buttonNames)
+if love.graphics then
+	MainMenu.font = love.graphics.newFont(36)
+	MainMenu.menu = Menu.create(love.graphics.getWidth()/2, 250, 5, buttonNames)
+else
+	MainMenu.menu = Menu.create(0, 0, 5, buttonNames)
+end
 
 function MainMenu.update(dt)
 	MainMenu.menu:update(dt)

src/gamestates/newGameMenu.lua
@@ -6,7 +6,7 @@ local NewGameMenu = {}
 setmetatable(NewGameMenu, GameState)
 
 local buttonNames = {"Single Player", "COOP", "Allied", "VS"}
-NewGameMenu.menu = Menu.create(love.graphics.getWidth()/2, 250, 5, buttonNames)
+if love.graphics then NewGameMenu.menu = Menu.create(love.graphics.getWidth()/2, 250, 5, buttonNames) end
 
 function NewGameMenu.update(dt)
 	NewGameMenu.menu:update(dt)

src/particles.lua
@@ -1,8 +1,6 @@
 local Particles = {}
 Particles.__index = Particles
 
-local explosionImage = love.graphics.newImage("res/images/explosion.png")
-
 function Particles.create(worldInfo, location, data)
 	self = {}
 	setmetatable(self, Particles)
@@ -16,7 +14,7 @@ function Particles.create(worldInfo, location, data)
 	self.fixture = love.physics.newFixture(self.body, self.physicsShape)
 	self.fixture:setUserData(self)
 	self.fixture:setSensor(true)
-	self.image = explosionImage
+	self.image = love.graphics.newImage("res/images/explosion.png")
 	self.ox = 20
 	self.oy = 20
 	self.time = 0.3

commit 9fb1a2f34851d58f110884e1e28836978d50a16d

Fixed bug with small menus scrolling
This bug was introduced in 1a6d9ca9.

src/menu.lua
@@ -13,7 +13,6 @@ function Menu.create(x, y, size, buttons, camera)
 	self.textHeight = size * 8
 	self.x = x - self.buttonWidth/2
 	self.y = y
-	self.visibleHeight = 0
 	self.buttons = buttons
 	self.scrollY = 0
 	self.scrollVelocity = 0
@@ -46,7 +45,7 @@ function Menu:update(dt)
 	self.scrollVelocity = self.scrollVelocity * 0.98
 
 	local menuHeight = self:getHeight()
-	if menuHeight > self.visibleHeight then
+	if self.visibleHeight and menuHeight > self.visibleHeight then
 		-- Reset scroll position and velocity if we hit the top or bottom of
 		-- the menu.
 		-- Top of the menu:
c82c745a8ed969f9d78799394c6db341ca0992b0
Jordan Christiansen <Jordan.Christiansen@target.com>
2017-12-10 19:37:05 -0600

    Added backgrounds to menus
    This horrendous code probably introduced some bugs in menus, especially
    when it comes to the mouse input side of things.
    
    fixes #121; fixes #124

--- src/menu.lua
+++ src/menu.lua
@@ -7,29 +7,34 @@ function Menu.create(x, y, size, buttons, camera)
 	self = {}
 	setmetatable(self, Menu)
 
-	self.buttonWidth = size * 100
-	self.buttonHeight = size * 10
-	self.buttonSpacing = size * 15
-	self.textHeight = size * 8
-	self.x = x - self.buttonWidth/2
+	self.x = x
 	self.y = y
+	self.width = size * 100
+	self.buttonWidth = size * 90
+	self.buttonHeight = size * 10
+	self.buttonSpacing = size * 5
+	self.buttonMargin = (self.width - self.buttonWidth) / 2
+	self.textHeight = size * 8
 	self.buttons = buttons
 	self.scrollY = 0
 	self.scrollVelocity = 0
 	self.selectedButton = 0
 	self.camera = camera
 	if love.graphics then self.font = love.graphics.newFont(size * 7) end
+	if love.graphics then self.visibleHeight = love.graphics.getHeight() - self.y - 20 end
 
 	return self
 end
 
 function Menu:getButtonAt(x, y)
-	if x > self.x and x < self.x + self.buttonWidth and y > self.y then
-		local yRef = y - self.y + self.scrollY
-		local index = math.floor(yRef/self.buttonSpacing) + 1
-		local remainder = yRef % self.buttonSpacing
+	if x > self.x - self.width / 2 + self.buttonMargin
+	   and x < self.x + self.buttonMargin + self.buttonWidth
+	   and y > self.y then
+		local yRef = y - self.y - self.buttonMargin + self.scrollY
+		local index = math.floor(yRef/(self.buttonHeight + self.buttonSpacing)) + 1
+		local remainder = yRef % (self.buttonHeight + self.buttonSpacing)
 		if index > 0 and index <= #self.buttons and
-		   remainder < self.buttonSpacing then
+		   remainder < self.buttonHeight then
 			return index
 		end
 	end
@@ -37,7 +42,9 @@ function Menu:getButtonAt(x, y)
 end
 
 function Menu:getHeight()
-	return #self.buttons * self.buttonSpacing
+	return self.buttonMargin * 2
+	       + (#self.buttons - 1) * self.buttonHeight
+	       + (#self.buttons - 1) * self.buttonSpacing
 end
 
 function Menu:update(dt)
@@ -49,16 +56,16 @@ function Menu:update(dt)
 		-- Reset scroll position and velocity if we hit the top or bottom of
 		-- the menu.
 		-- Top of the menu:
-		if self.scrollY < 0 then
-			self.scrollY = 0
+		if self.scrollY < 20 then
+			self.scrollY = 20
 			if self.scrollVelocity < 0 then
 				self.scrollVelocity = 0
 			end
 		end
 
 		-- Bottom of the menu:
-		if self.scrollY > menuHeight - self.visibleHeight then
-			self.scrollY = menuHeight - self.visibleHeight
+		if self.scrollY > menuHeight - self.visibleHeight - 20 then
+			self.scrollY = menuHeight - self.visibleHeight - 20
 			if self.scrollVelocity > menuHeight then
 				self.scrollVelocity = 0
 			end
@@ -77,29 +84,35 @@ function Menu:update(dt)
 end
 
 function Menu:draw()
-	self.visibleHeight = love.graphics.getHeight() - self.y
+	love.graphics.push("all")
 	local x, y
 	if self.camera then
-		love.graphics.setScissor(self.camera:getScissor())
 		x, y = self.camera:getScissor()
-		x = x + self.x
+		x = x + self.x - self.width / 2
 		y = y + self.y
 	else
-		x = self.x
+		x = self.x - self.width / 2
 		y = self.y
 	end
 
+	love.graphics.setScissor(x, y, self.width, self.visibleHeight)
+	love.graphics.setColor(200, 200, 200)
+	love.graphics.rectangle(
+		"fill",
+		x, y,
+		self.width, math.min(self:getHeight() + self.buttonMargin * 2, self.visibleHeight))
+
 	for i, button in ipairs(self.buttons) do
 		if i == self.selectedButton then
-			love.graphics.setColor(180, 180, 180)
+			love.graphics.setColor(150, 150, 150)
 		else
 			love.graphics.setColor(100, 100, 100)
 		end
 		
 		love.graphics.rectangle(
 			"fill",
-			self.x,
-			self.y + self.buttonSpacing * (i - 1) - self.scrollY,
+			x + self.buttonMargin,
+			y + self.buttonMargin + self.buttonHeight * (i - 1) + self.buttonSpacing * (i - 1) - self.scrollY,
 			self.buttonWidth, self.buttonHeight
 		)
 		love.graphics.setColor(255, 255, 255)
@@ -107,16 +120,14 @@ function Menu:draw()
 		love.graphics.setFont(self.font)
 		love.graphics.print(
 			self.buttons[i],
-			self.x + 10,
-			self.y + 75 * (i - 1) + (self.buttonHeight - self.textHeight)/2 - self.scrollY,
+			x + self.buttonMargin + 10,
+			y + self.buttonMargin + self.buttonHeight * (i - 1) + self.buttonSpacing * (i - 1) + (self.buttonHeight - self.textHeight)/2 - self.scrollY,
 			0, 1, 1, 0, 0, 0, 0
 		)
 		love.graphics.setFont(previousFont)
 	end
-	
-	if self.camera then
-		love.graphics.setScissor()
-	end
+
+	love.graphics.pop()
 end
 
 function Menu:resize(w, h)

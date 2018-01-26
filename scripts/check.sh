f835c2b35070c794219bb1b628be8120fc9374b9
Jordan Christiansen <xordspar0@gmail.com>
2018-01-26 12:23:39 -0600

    Add an option -f to check.sh
    The -f option to scripts/check.sh will force falling back to luac -p,
    even if luacheck is installed.

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-if command -v luacheck >/dev/null 2>&1; then
-	cmd="luacheck --std=luajit+love"
-else
-	echo "luacheck is not installed. Falling back to 'luac -p'."
+if [ "$1" != "-f" ]; then
+	if command -v luacheck >/dev/null 2>&1; then
+		cmd="luacheck --std=luajit+love"
+	else
+		echo "luacheck is not installed. Falling back to 'luac -p'."
+	fi
+fi
+
+if [ -z "$cmd" ]; then
 	cmd="luac -p"
 fi
 

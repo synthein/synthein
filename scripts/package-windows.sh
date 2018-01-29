c80adc28ef7b7e0b78e7092c2d69654326e4b1e7
Jordan Christiansen <xordspar0@gmail.com>
2018-01-29 13:21:15 -0600

    Clean up and normalize package scripts
    - Non-environment variables are lower case
    - Don't make directories that already exist
    - Use the same variables in every script

--- scripts/package-windows.sh
+++ scripts/package-windows.sh
@@ -1,26 +1,32 @@
 #!/bin/sh
 set -e
 
-ROOT_DIR=$(pwd)
+root_dir="$(pwd)"
+build_dir="${root_dir}/build"
+build_file="${build_dir}/synthein-windows-${SYNTHEIN_VERSION}.zip"
 
-if [ ! -f "${ROOT_DIR}/build/synthein-${SYNTHEIN_VERSION}.love" ]; then
+if [ ! -f "${build_dir}/synthein-${SYNTHEIN_VERSION}.love" ]; then
 	echo "Need to build the .love file first."
 	exit 1
 fi
 
 echo "Getting Windows LÖVE binary."
-cd "${ROOT_DIR}/build"
+cd "${build_dir}"
 curl -L -O "http://bitbucket.org/rude/love/downloads/love-${LOVE_VERSION}-win64.zip"
 unzip "love-${LOVE_VERSION}-win64.zip"
 
 echo "Building Windows package."
-mkdir "${ROOT_DIR}/build/synthein-windows"
-cd "${ROOT_DIR}/build/synthein-windows"
+windows_build_dir="${build_dir}/synthein-windows"
+if [ -d "${windows_build_dir}" ]; then
+	rm -r "${windows_build_dir}"
+fi
+mkdir "${windows_build_dir}"
+cd "${windows_build_dir}"
 echo $SYNTHEIN_VERSION > SYNTHEIN_VERSION
 cat ../love-${LOVE_VERSION}-win64/love.exe ../synthein-${SYNTHEIN_VERSION}.love > synthein.exe
 cp ../love-${LOVE_VERSION}-win64/*.dll ../love-${LOVE_VERSION}-win64/license.txt ./
 
-cd "${ROOT_DIR}/build"
-zip -r "synthein-windows-${SYNTHEIN_VERSION}.zip" synthein-windows/
+cd "${build_dir}"
+zip -r "${build_file}" synthein-windows/
 
-echo "Built $(find . -name synthein-windows*.zip)."
+echo "Built ${build_file}."

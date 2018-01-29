2e4c904fe9c21accf23d49d59ddaf84a15cfd470
Jordan Christiansen <Jordan.Christiansen@target.com>
2018-01-29 13:21:15 -0600

    Clean up and normalize package scripts
    - Non-environment variables are lower case
    - Don't make directories that already exist
    - Use the same variables in every script

--- scripts/package-love.sh
+++ scripts/package-love.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 set -e
 
-if [ ! -d build ]; then
-	mkdir build
+root_dir="$(pwd)"
+build_dir="${root_dir}/build"
+build_file="${build_dir}/synthein-${SYNTHEIN_VERSION}.love"
+
+if [ ! -d "${build_dir}" ]; then
+	mkdir "${build_dir}"
 fi
 
 cd src
-zip -9 -r "../build/synthein-${SYNTHEIN_VERSION}.love" .
+zip -9 -r "${build_file}" .
 
-cd ..
-echo "Built $(find . -name synthein*.love)."
+cd "${root_dir}"
+echo "Built ${build_file}."

273b05abd06de87fc9fbd54d7cee8057f8c1e73b
Jordan Christiansen <xordspar0@gmail.com>
2018-04-05 11:06:23 -0500

    Added lunatest

---
+++ src/tests/init.lua
@@ -0,0 +1,10 @@
+local lunatest = require("lunatest")
+
+for _, file in ipairs(love.filesystem.getDirectoryItems("tests")) do
+  modname = file:match("^(test_.*)%.lua$")
+  if modname then
+    lunatest.suite("tests." .. modname)
+  end
+end
+
+lunatest.run()

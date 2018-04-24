748eebbcaa0d2e87f0d693920cd2c693efbd500d
Jordan Christiansen <xordspar0@gmail.com>
2018-04-24 16:56:17 -0500

    Don't allow empty game save names

--- src/gamesave.lua
+++ src/gamesave.lua
@@ -20,16 +20,20 @@ end
 -- Returns boolean success status and (optionally) a message explaining why
 -- there was a failure.
 function Gamesave.save(saveName, world)
-	local fileString = SceneParser.saveScene(world)
+	if saveName == "" then
+		return false, "save name cannot be empty"
+	end
+
+	local fileContents = SceneParser.saveScene(world)
 
 	if not love.filesystem.isDirectory(saveDir) then
 		local ok = love.filesystem.createDirectory(saveDir)
 		if not ok then
-			return false, "Failed to create save directory."
+			return false, "failed to create save directory"
 		end
 	end
 
-	return love.filesystem.write(saveDir .. saveName .. ".txt", fileString)
+	return love.filesystem.write(saveDir .. saveName .. ".txt", fileContents)
 end
 
 return Gamesave

a35ba50c85e2aaf3a923c0a6974383446cf5fd2c
Jordan Christiansen <xordspar0@gmail.com>
2018-11-26 09:51:46 -0600

    Shorten all lines that are too long

--- src/conf.lua
+++ src/conf.lua
@@ -12,12 +12,12 @@ function love.conf(t)
 	t.window.minwidth = 1                -- Minimum window width if the window is resizable (number)
 	t.window.minheight = 1               -- Minimum window height if the window is resizable (number)
 	t.window.fullscreen = false          -- Enable fullscreen (boolean)
-	t.window.fullscreentype = "desktop"  -- Standard Choose between "desktop" fullscreen or "exclusive" fullscreen mode (string)
+	t.window.fullscreentype = "desktop"  -- Choose between "desktop" fullscreen or "exclusive" fullscreen mode (string)
 	t.window.vsync = true                -- Enable vertical sync (boolean)
 	t.window.msaa = 2                    -- The number of samples to use with multi-sampled antialiasing (number)
 	t.window.display = 1                 -- Index of the monitor to show the window in (number)
-	t.window.highdpi = false             -- Enable high-dpi mode for the window on a Retina display (boolean). Added in 0.9.1
-	t.window.srgb = false                -- Enable sRGB gamma correction when drawing to the screen (boolean). Added in 0.9.1
+	t.window.highdpi = false             -- Enable high-dpi mode for the window on a Retina display (boolean)
+	t.window.srgb = false                -- Enable sRGB gamma correction when drawing to the screen (boolean)
 
 	t.modules.audio = true               -- Enable the audio module (boolean)
 	t.modules.event = true               -- Enable the event module (boolean)

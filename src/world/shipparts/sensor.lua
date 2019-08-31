b613e3721292666188e282b73213a2b5db29ed97
randomguyeatspotato <randomguyeatspotato@gmail.com>
2019-08-31 05:52:15 -0500

    Add fixtures to all of the sensors.

--- src/world/shipparts/sensor.lua
+++ src/world/shipparts/sensor.lua
@@ -1,12 +1,29 @@
+local PhysicsReferences = require("world/physicsReferences")
+
 local Sensor = class()
 
-function Sensor:__create()
-
+function Sensor:__create(size)
+	self.size = size
 	return self
 end
 
 function Sensor:update(moduleInputs, location)
 
+end
+
+function Sensor:addFixtures(body, x, y)
+	local shape = love.physics.newCircleShape(x, y, self.size)
+	self.fixture = love.physics.newFixture(body, shape)
+	PhysicsReferences.setFixtureType(self.fixture, "camera")
+
+	local userData = {
+		collision = function() end
+	}
+	self.fixture:setUserData(userData)
+end
+
+function Sensor:removeFixtures()
+	self.fixture:destroy()
 end
 
 return Sensor

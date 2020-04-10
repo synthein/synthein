cf9064c12f3f821fc9223b6c3bb46ba8f3697f5e
randomguyeatspotato <randomguyeatspotato@gmail.com>
2020-04-10 01:15:32 -0500

    Makes the repairBlock go black when it sees something.

--- src/world/shipparts/repairBlock.lua
+++ src/world/shipparts/repairBlock.lua
@@ -1,7 +1,10 @@
+local Sensor = require("world/shipparts/sensor")
+
 -- Utilities
 local LocationTable = require("locationTable")
 
-local RepairBlock = class(require("world/shipparts/part"))
+local Part = require("world/shipparts/part")
+local RepairBlock = class(Part)
 
 function RepairBlock:__create()
     local image = love.graphics.newImage("res/images/repairBlock.png")
@@ -16,6 +19,9 @@ function RepairBlock:__create()
 
 	local modules = self.modules
 
+    local sensor = Sensor(2)
+    modules["sensor"] = sensor
+
 	function self.userData:draw(fixture, scaleByHealth)
 		if scaleByHealth then
 			c = modules.health:getScaledHealh()
@@ -23,6 +29,10 @@ function RepairBlock:__create()
 		else
 			love.graphics.setColor(1, 1, 1, 1)
 		end
+        --print(#sensor:getBodyList())
+        if next(sensor.bodyList) then
+            love.graphics.setColor(0,0,0, 1)
+        end
 		local x, y, angle = LocationTable(fixture, self.location):getXYA()
 		love.graphics.draw(
 			image,
@@ -30,6 +40,17 @@ function RepairBlock:__create()
 			1/self.width, -1/self.height, self.width/2, self.height/2)
 		love.graphics.setColor(1, 1, 1, 1)
 	end
+end
+
+function RepairBlock:addFixtures(body)
+	Part.addFixtures(self, body)
+    local l = self.location
+	self.modules.sensor:addFixtures(body, l[1], l[2])
+end
+
+function RepairBlock:removeFixtures()
+	Part.removeFixtures(self)
+	self.modules.sensor:removeFixtures()
 end
 
 return RepairBlock

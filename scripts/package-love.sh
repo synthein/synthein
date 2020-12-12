e4cb323299b411667e12c16d43d33d55428348e3
Jordan Christiansen <xordspar0@gmail.com>
2020-12-12 10:44:13 -0600

    Simplify package-love.sh (#226)
    There were some lines that didn't do anything, and overall the script
    was unintuitive. This should be simpler and cleaner.

--- scripts/package-love.sh
+++ scripts/package-love.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 set -e
 
-root_dir="$(pwd)"
-build_dir="${root_dir}/build"
-build_file="${build_dir}/synthein-${SYNTHEIN_VERSION}.love"
+build_file="${PWD}/build/synthein-${SYNTHEIN_VERSION}.love"
 
-if [ ! -d "${build_dir}" ]; then
-	mkdir "${build_dir}"
-fi
+mkdir -p "$(dirname "$build_file")"
 
 cd src
 zip -9 -r "${build_file}" .
 
-cd "${root_dir}"
 echo "Built ${build_file}."

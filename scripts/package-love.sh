5a5c9b5a6c7a5800d98b8fb22766ecf328e5402c
Jordan Christiansen <xordspar0@gmail.com>
2021-05-05 22:49:50 -0500

    Show version info in the main menu
    Fixes #187

--- scripts/package-love.sh
+++ scripts/package-love.sh
@@ -8,4 +8,8 @@ mkdir -p "$(dirname "$build_file")"
 cd src
 zip -9 -r "${build_file}" .
 
+cd ../build
+echo "return \"${SYNTHEIN_VERSION}\"" > version.lua
+zip -9 "${build_file}" version.lua
+
 echo "Built ${build_file}."

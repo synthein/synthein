db66072b395d4bd69e6cfe3c294130e54872ce3c
drake <randomguyeatspotato@gmail.com>
2021-05-06 19:39:20 -0500

    Clean up Building.draw function

--- src/building.lua
+++ src/building.lua
@@ -83,160 +83,26 @@ function Building.getStrengthTable(part, partSide)
 	return strength
 end
 
+local offsetTable = {{0, .5}, {-.5, 0}, {0, -.5}, {.5, 0}}
+
 function Building:draw()
 	if self.annexeePart and self.annexeePartSide then
-		local x, y, partAngle = self.annexeePart:getWorldLocation():getXYA()
-		local angle = self.annexeePartSide * math.pi/2 + partAngle
-		local offsetX, offsetY = Util.vectorComponents(.5, angle)
-		self.camera:draw(self.pointerImage,
-						 x + offsetX,
-						 y + offsetY,
-						 angle,
-						 1/20, 1/20, self.pointerWidth/2, self.pointerWidth/2)
+		local body = self.annexeePart.fixture:getBody()
+
+		local l = self.annexeePart.location
+
+		local side = (self.annexeePartSide + l[3] - 2) % 4 + 1
+		local offsetX, offsetY = unpack(offsetTable[side])
+
+		local x, y = body:getWorldPoint(l[1] + offsetX, l[2] + offsetY)
+		local angle = body:getAngle()
+
+		self.camera:draw(
+			self.pointerImage,
+			x, y, angle,
+			1/20, 1/20,
+			self.pointerWidth/2, self.pointerWidth/2)
 	end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---function Building:pressed(mouseWorldX, mouseWorldY)
---	if self.mode == 1 then
---
---	elseif self.mode == 3 then
---		self.structure, self.structurePart =
---		self.world:getStructure(mouseWorldX,mouseWorldY)
---		self.mode = 4
---		if not self.structure or (self.structure.corePart and self.structure.corePart:getTeam() ~= self.team) then
---			return true --end build
---		else
---			return false --don't end build
---		end
---	end
---end
---
---function Building:released(cursorX, cursorY)
---	local withinPart = true
---	if self.mode == 2 then
---		withinPart, self.annexeePartSide =
---			self.annexee:withinPart(self.annexee:findPart(self.annexeePart), cursorX, cursorY)
---		if not self.annexeePart.connectableSides[self.annexeePartSide] then
---			return true --end build
---		end
---		if not withinPart then
---			self.mode = 3
---		end
---		return false  --don't end build
---
---	elseif self.mode == 4 then
---		withinPart, self.structurePartSide =
---			self.structure:withinPart(self.structure:findPart(self.structurePart), cursorX, cursorY)
---		if withinPart then
---			return false  --don't end build
---		end
---		if self.structure and self.annexee
---			and self.structurePart.connectableSides[self.structurePartSide]
---			and self.structure ~= self.annexee then
---			world:annex(self.annexee, self.annexeePart, self.annexeePartSide, self.annexeeIndex,
---						self.structure, self.structurePart, self.structurePartSide)
---		end
---		self.structure, self.annexee = nil
---		return true --end build
---	end
---end
---
---function Building:draw(mouseWorldX, mouseWorldY)
---	local withinPart
---	if self.mode == 2 then
---		local partX, partY, partAngle = self.annexee:getAbsPartCoords(self.annexee:findPart(self.annexeePart))
---		withinPart, self.annexeePartSide =
---			self.annexee:withinPart(self.annexee:findPart(self.annexeePart), cursorX, cursorY)
---		if withinPart then
---			self.annexeePartSide = nil
---		end
---		self:drawCircle(
---			partX, partY, partAngle,
---			self.annexeePartSide, self.annexeePart.connectableSides
---		)
---	elseif self.mode == 4 and self.structure then
---		local partX, partY, partAngle = self.structure:getAbsPartCoords(self.structure:findPart(self.structurePart))
---		withinPart, self.structurePartSide =
---			self.structure:withinPart(self.structure:findPart(self.structurePart), cursorX, cursorY)
---		if withinPart then
---			self.structurePartSide = nil
---		end
---		self:drawCircle(
---			partX, partY, partAngle,
---			self.structurePartSide, self.structurePart.connectableSides
---		)
---	end
---	if self.annexeePart and self.annexeePart.connectableSides[self.annexeePartSide] then
---		local x, y, partAngle = self.annexee:getAbsPartCoords(self.annexee:findPart(self.annexeePart))
---		local offsetX, offsetY -- move the cursor the side that we are selecting
---		local angle = self.annexeePartSide * math.pi/2 + partAngle
---		offsetX, offsetY = Util.vectorComponents(10, angle)
---		self.camera:draw(
---						 self.pointerImage,
---						 x + offsetX,
---						 y + offsetY,
---						 angle,
---						 1, 1, self.pointerWidth/2, self.pointerWidth/2
---		)
---	end
---	if self.mode == 4 and self.structurePart.connectableSides[self.structurePartSide] then
---		local x, y, partAngle = self.structure:getAbsPartCoords(self.structure:findPart(self.structurePart))
---		local offsetX, offsetY -- move the cursor the side that we are selecting
---		local angle = self.structurePartSide * math.pi/2 + partAngle
---		offsetX, offsetY = Util.vectorComponents(10, angle)
---		self.camera:draw(
---			self.pointerImage,
---			x + offsetX,
---			y + offsetY,
---			angle,
---			1, 1, self.pointerWidth/2, self.pointerWidth/2
---		)
---	end
---end
---
---function Building:drawCircle(centerX, centerY, angle, highlightedSide, connectableSides)
---	strength = {}
---	for i = 1, 4 do
---		if connectableSides[i] then
---			if i == highlightedSide then
---				table.insert(strength, 2)
---			else
---				table.insert(strength, 1)
---			end
---		else
---			table.insert(strength, 0)
---		end
---	end
---	strengthX = strength[2]
---	strength[2] = strength[4]
---	strength[4] = strengthX
---	self.camera:drawCircleMenu(centerX, centerY, angle, 10, strength)
---
---	love.graphics.setColor(1, 1, 1)
---end
 
 return Building

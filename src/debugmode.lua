b57e97d4f9526317adbd7833fc7ce838935b9be8
Jordan Christiansen <xordspar0@gmail.com>
2022-01-15 16:38:25 -0600

    Draw sensors in debug mode

--- src/debugmode.lua
+++ src/debugmode.lua
@@ -1,4 +1,9 @@
+local Camera = require("camera")
+local PhysicsReferences = require("world/physicsReferences")
+local Settings = require("settings")
 local Spawn = require("world/spawn")
+
+local lume = require("vendor/lume")
 
 local Debug = {}
 
@@ -8,6 +13,8 @@ function Debug.create(world, players)
 	self.players = players
 	self.on = false
 	self.spawn = false
+
+	self.drawSensors = Camera.wrap(self.drawSensors, true)
 	return self
 end
 
@@ -37,6 +44,8 @@ function Debug:draw()
 				shipParts = 0
 			end
 
+			self.drawSensors({world = self.world, camera = player.camera}, player)
+
 			-- Print the debug information.
 			player.camera:print(
 				string.format("%.0f %.0f\n",
@@ -54,6 +63,48 @@ function Debug:draw()
 			)
 		end
 	end
+end
+
+-- Show invisible sensors.
+function Debug:drawSensors(player)
+	love.graphics.push("all")
+	love.graphics.setColor(0.5, 0.5, 0.5, 0.5)
+	love.graphics.setLineWidth(2/Settings.PARTSIZE)
+
+	local sensors = {}
+	local a, b, c, d = player.camera:getWorldBorder()
+	self.world.physics:queryBoundingBox(a, b, c, d, function(fixture)
+		local category = fixture:getFilterData()
+		if category == PhysicsReferences.getCategory("sensor")
+			 or category == PhysicsReferences.getCategory("shield") then
+			table.insert(sensors, fixture)
+		end
+		return true
+	end)
+
+	for _, sensor in ipairs(sensors) do
+		local shape = sensor:getShape()
+		local type = shape:getType()
+
+		if type == "circle" then
+			local x, y = shape:getPoint()
+			x, y = sensor:getBody():getWorldPoint(x, y)
+			love.graphics.circle("line", x, y, shape:getRadius())
+		elseif type == "polygon" then
+			local local_points = {shape:getPoints()}
+			local world_points = {}
+			for i = 1, #local_points, 2 do
+				local x, y = sensor:getBody():getWorldPoint(local_points[i], local_points[i+1])
+				table.insert(world_points, x)
+				table.insert(world_points, y)
+			end
+			love.graphics.polygon("line", world_points)
+		else
+			error("Unhandled shape type \"" .. type .. "\"")
+		end
+	end
+
+	love.graphics.pop()
 end
 
 function Debug:update(dt)

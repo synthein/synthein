60b2c7d757e5f966c64a219e4fb73bf893796d5b
drake <randomguyeatspotato@gmail.com>
2022-06-04 19:07:53 -0500

    Draw text. Remove more code.

--- src/saveMenu.lua
+++ src/saveMenu.lua
@@ -4,9 +4,10 @@ local SaveMenu = class()
 function SaveMenu:__create()
 
 	self.y = 250
-	self.width = 100
-	self.height = 40
+	self.width = 200
+	self.height = 60
 
+	self.saveName = "filename"
 
 	--if love.graphics then self.font = love.graphics.newFont(size * 7) end
 	--if love.graphics then self.visibleHeight = love.graphics.getHeight() - self.y - self.buttonMargin end
@@ -14,20 +15,8 @@ function SaveMenu:__create()
 	return self
 end
 
-function SaveMenu:getButtonAt(x, y)
-	local menuCenter = love.graphics.getWidth() / 2
-	if x > menuCenter - self.buttonWidth / 2
-	   and x < menuCenter + self.buttonWidth / 2
-	   and y > self.y then
-		local yRef = y - self.y - self.buttonMargin + self.scrollY
-		local index = math.floor(yRef/(self.buttonHeight + self.buttonSpacing)) + 1
-		local remainder = yRef % (self.buttonHeight + self.buttonSpacing)
-		if index > 0 and index <= #self.buttons and
-		   remainder < self.buttonHeight then
-			return index
-		end
-	end
-	return nil
+function getFileString()
+	return saveName
 end
 
 function SaveMenu:draw()
@@ -43,11 +32,30 @@ function SaveMenu:draw()
 	love.graphics.setColor(0.4, 0.4, 0.4)
 	love.graphics.rectangle("fill", x+5, y+5, self.width-10, self.height-10)
 
+
+	love.graphics.setColor(0, 0, 0)
+	--love.graphics.print("Type a name to use for your save, then press enter:", screen_width/2-150, 60)
+	love.graphics.print(self.saveName, x+10, y+10)
+
 	love.graphics.pop()
 end
 
 function SaveMenu:keypressed(key)
 	-- Add main text modifing code here
+
+	if key == "backspace" then
+		-- The string is utf-8 encoded, so the last character of the string
+		-- could be multiple bytes.
+		local byteoffset = utf8.offset(saveName, -1)
+		if byteoffset then
+			saveName = saveName:sub(1, byteoffset - 1)
+		end
+	elseif key == "return" then
+		typingSaveName = false
+	elseif key == "escape" then
+		saveName = ""
+		typingSaveName = false
+	end
 end
 
 function SaveMenu:mousemoved(x, y)

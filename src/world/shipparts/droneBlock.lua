667e42b3fdc56783da7b481600ae2b961d010df6
randomguyeatspotato <randomguyeatspotato@gmail.com>
2022-09-29 17:24:50 -0500

    Ai to drone termonology (#300)
    * Swap names in main files. Working.
    
    * Change scene and ship files to use different letters.

--- src/world/shipparts/droneBlock.lua
+++ src/world/shipparts/droneBlock.lua
@@ -1,67 +1,67 @@
 -- Components
 local Hull = require("world/shipparts/hull")
 local Engine = require("world/shipparts/engine")
-local AI = require("world/shipparts/ai")
+local Drone = require("world/shipparts/drone")
 local Sensor = require("world/shipparts/sensor")
 
 -- Graphics
 local Draw = require("world/draw")
-local aiTeams = {-4, -3, -2, -1, 1, 2}
+local droneTeams = {-4, -3, -2, -1, 1, 2}
 local imageFunctions = {}
-for i, team in ipairs(aiTeams) do
-	imageFunctions[team] = Draw.createDrawBlockFunction(Draw.loadImage("ai" .. team))
+for i, team in ipairs(droneTeams) do
+	imageFunctions[team] = Draw.createDrawBlockFunction(Draw.loadImage("drone" .. team))
 end
 
 -- Class Setup
 local Part = require("world/shipparts/part")
-local AIBlock = class(Part)
+local DroneBlock = class(Part)
 
-AIBlock.image = Draw.loadImage("ai" .. 1)
+DroneBlock.image = Draw.loadImage("drone" .. 1)
 
-function AIBlock:__create(team, leader)
+function DroneBlock:__create(team, leader)
 	self.modules["hull"] = Hull(imageFunctions[team], 10)
 	self.type = "control"
 
 	self.modules["engine"] = Engine(1, 10, 10)
 	self.modules["sensor"] = Sensor(200)
-	self.modules["ai"] = AI(team)
+	self.modules["drone"] = Drone(team)
 	self.leader = leader
 end
 
-function AIBlock:postCreate(references)
+function DroneBlock:postCreate(references)
 	if self.leader and type(self.leader) == "string" then
 		self.leader = references[self.leader]
 	end
 end
 
-function AIBlock:getTeam()
-	return self.modules.ai.team
+function DroneBlock:getTeam()
+	return self.modules.drone.team
 end
 
-function AIBlock:addFixtures(body)
+function DroneBlock:addFixtures(body)
 	Part.addFixtures(self, body)
 	self.modules.sensor:addFixtures(body, 0, 0)
 end
 
-function AIBlock:removeFixtures()
+function DroneBlock:removeFixtures()
 	Part.removeFixtures(self)
 	self.modules.sensor:removeFixtures()
 end
 
-function AIBlock:getOrders(body)
-	return self.modules.ai:getOrders(
+function DroneBlock:getOrders(body)
+	return self.modules.drone:getOrders(
 		self.worldInfo,
 		self.leader,
 		body,
 		self.modules.sensor:getBodyList())
 end
 
-function AIBlock:getMenu()
-	return self.modules.ai:getMenu()
+function DroneBlock:getMenu()
+	return self.modules.drone:getMenu()
 end
 
-function AIBlock:runMenu(i, body)
-	return self.modules.ai:runMenu(i, body)
+function DroneBlock:runMenu(i, body)
+	return self.modules.drone:runMenu(i, body)
 end
 
-return AIBlock
+return DroneBlock

b6eed1823d457f9bf15fbb8a5eec153a6cad0f97
randomguyeatspotato <randomguyeatspotato@gmail.com>
2022-10-31 18:09:49 -0500

    Move team to userdata of the body. (#304)
    * Move team to userdata of the body.
    
    * Clarified TODO

--- src/main.lua
+++ src/main.lua
@@ -40,7 +40,10 @@ function love.load()
 
 			if scene then
 				local InitWorld = require("gamestates/initWorld")
-				MainMenu.stackQueue:push(InitWorld).load(scene, {}, false)
+				--TODO {{true}} is a bandaid. Perminant solution required
+				-- No longer crashes with drones/players of team 1
+				-- Still crashes with drones/players of teams 2+
+				MainMenu.stackQueue:push(InitWorld).load(scene, {{true}}, false)
 			else
 				error(
 					"--scene must have an argument. You provided these arguments: "

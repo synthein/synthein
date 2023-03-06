4750735a808f3faaf6466e9135c66c6e9d63eed5
Jordan Christiansen <xordspar0@gmail.com>
2023-03-06 17:07:09 -0600

    Fix tests (#347)
    - The player test doesn't make sense anymore. There is no
      drawWorldObjects function anymore.
    - InGame.load() changed, so the inGame test needed to update how it
      called that function.
    - The inGame test caught a real regression! If you resize the window by
      going to full screen during inGame, the game would crash. Now it's
      fixed.

--- src/tests/test_player.lua
+++ src/tests/test_player.lua
@@ -6,26 +6,4 @@ local PhysicsReferences = require("world/physicsReferences")
 
 local t = {}
 
-function t.test_player_should_draw_world_objects()
-	local drawCalls = 0
-	local block = {draw = function() drawCalls = drawCalls + 1 end}
-	local blockFixture = {
-		getUserData = function() return block end,
-		getFilterData = function() return PhysicsReferences.categories.general end,
-	}
-
-	local player = {
-		camera = Camera.create(),
-		world = {
-			physics = {
-				queryBoundingBox = function(_, _, _, _, _, fn) fn(blockFixture) end,
-			}
-		}
-	}
-
-	Player.drawWorldObjects(player, false)
-
-	test.assert_equal(1, drawCalls)
-end
-
 return t

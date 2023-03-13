40e3ceb75840687604a63e248b2a3e125895d138
Jordan Christiansen <xordspar0@gmail.com>
2023-03-13 17:24:58 -0500

    Add an integration test (#350)
    This is the first of many integration tests, where we test features of
    the game while it is running via it's interface(s). The first test
    starts the game in a scene. It should check to see if it fails and stop
    the game once the tests are complete, but that's not yet implemented.

--- src/tests/stub.lua
+++ src/tests/stub.lua
@@ -3,6 +3,7 @@ return function()
   return setmetatable(stub, {
     __index = function() return stub end,
     __call = function() return stub, stub end,
+    __add = function() return 1 end,
     __sub = function() return 1 end,
     __mul = function() return 1 end,
     __div = function() return 1 end,

d04f3b940bac81ed1d2863126d1bcc565f8a91dc
randomguyeatspotato <randomguyeatspotato@gmail.com>
2023-03-13 17:21:03 -0500

    Moved drawing for building into selection and cleanup. (#349)

--- src/building.lua
+++ src/building.lua
@@ -1,11 +1,5 @@
-local StructureMath = require("world/structureMath")
-
 local Building = {}
 Building.__index = Building
-local pointerImage = love.graphics.newImage("res/images/pointer.png")
-local pointerWidth = pointerImage:getWidth()
-local pointerWidth = pointerImage:getWidth()
-
 function Building.create()
 	local self = {}
 	setmetatable(self, Building)
@@ -24,11 +18,7 @@ function Building:setAnnexee(structure, part)
 	self.annexeePart = part
 	self.mode = 2
 
-	local annexeeX = part.location[1]
-	local annexeeY = part.location[2]
-	local annexeeSide = part.location[3]
-
-	self.annexeeBaseVector = {annexeeX, annexeeY, annexeeSide}
+	self.annexeeBaseVector = {unpack(part.location)}
 	self.body = self.annexeePart.modules["hull"].fixture:getBody()
 end
 
@@ -40,11 +30,7 @@ function Building:setStructure(structure, part)
 	self.structurePart = part
 	self.mode = 4
 
-	local structureOffsetX = part.location[1]
-	local structureOffsetY = part.location[2]
-	local structureSide = part.location[3]
-
-	self.structureVector = {structureOffsetX, structureOffsetY, structureSide}
+	self.structureVector = {unpack(part.location)}
 
 	return false
 end
@@ -63,22 +49,6 @@ function Building:setSide(partSide)
 				self.structureVector)
 		end
 		self.mode = 5
-	end
-end
-
-function Building:draw()
-	local body = self.body
-	local vec = self.annexeeBaseVector
-	if body and vec and self.mode > 2 then
-		local l = StructureMath.addDirectionVector(vec, vec[3], .5)
-		local x, y = body:getWorldPoint(l[1], l[2])
-		local angle = body:getAngle()
-
-		love.graphics.draw(
-			pointerImage,
-			x, y, angle,
-			1/20, 1/20,
-			pointerWidth/2, pointerWidth/2)
 	end
 end
 

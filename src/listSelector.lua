1af9495958c13820ef2cb10740f74310cb9c88f4
drake <randomguyeatspotato@gmail.com>
2023-07-03 17:42:59 -0500

    Gets the canvas system setup.

--- src/listSelector.lua
+++ src/listSelector.lua
@@ -15,6 +15,8 @@ function ListSelector:__create(size, x, y, width, height, list)
 	self.width = width
 	self.height = height
 	
+	self.size = size
+	
 	self.horRef = 0
 	self.verRef = 0
 	self.horOff = 0
@@ -23,19 +25,53 @@ function ListSelector:__create(size, x, y, width, height, list)
 	self.scrollY = 0
 	self.scrollTo = 0
 	
-	local canvas = love.graphics.newCanvas(width, size * #list)
+	self.font = love.graphics.newFont(size * 0.6)
 
-	--self.buttonWidth = size * 90
-	--self.buttonHeight = size * 10
-	--self.buttonSpacing = size * 5
-	--self.buttonMargin = (self.width - self.buttonWidth) / 2
-	
-	self.setList(list)
-	
+	self:setList(list)
+	self.visableCanvas = love.graphics.newCanvas(width, height)
+
 	return self
 end
 
 function ListSelector:setList(list)
+	local size = self.size
+	local border = size/8
+	local doubleBorder = size/4
+	
+	-- Canvases must have nonzero dimensions.
+	self.buttonCanvas = love.graphics.newCanvas(self.width, self.size * #list + 1)
+	love.graphics.setCanvas(self.buttonCanvas)
+
+	love.graphics.clear(0.8, 0.8, 0.8)
+	love.graphics.setColor(0.4, 0.4, 0.4)
+	for i, name in ipairs(list) do
+		love.graphics.rectangle(
+			"fill",
+			border, (i-1) * size + border,
+			self.width - size/4, size - doubleBorder)
+	end
+	
+	self.textCanvas = love.graphics.newCanvas(self.width, size * #list + 1)
+	love.graphics.setCanvas(self.textCanvas)
+	
+	local previousFont = love.graphics.getFont()
+	love.graphics.setFont(self.font)
+
+	love.graphics.setColor(0, 0, 0)
+	for i, name in ipairs(list) do
+		love.graphics.print(name, self.size/4, (i-0.8) * self.size)
+	end
+
+	love.graphics.setFont(previousFont)
+	love.graphics.setCanvas()
+	love.graphics.setColor(1, 1, 1)
+	
+	--TODO do something with this
+	--[[
+	self.scrollMax = canvas:getHeight() - self.visibleHeight
+	if self.scrollMax < 0 then
+		self.scrollMax = 0
+	end--]]
 end
 
 --TODO rename variables
@@ -180,52 +216,6 @@ function ListSelector:pressed(x, y)
 	return self:loadfile(index)
 end
 
-function ListSelector:draw(viewPort)
-	local screenWidth = viewPort.width
-	local screenHeight = viewPort.height
-
-	local x = self.x + self.horRef * screenWidth  - self.width  * self.horOff
-	local y = self.y + self.verRef * screenHeight - self.height * self.verOff
-
-	love.graphics.rectangle(
-		"fill",
-		x, y,
-		self.width, self.height
-	)
---[[
-	love.graphics.setColor(1, 1, 1)
-	love.graphics.draw(self.mainCanvas, border, border)
-
-	local height = love.graphics.getHeight() - 2 * (bs)
-	local width  = love.graphics.getWidth()  - 2 * (bs)
-
-	local canvas = love.graphics.newCanvas(nameWidth, height)
-	love.graphics.setCanvas(canvas)
-
-	love.graphics.setColor(0.4, 0.4, 0.4)
-	-- When nothing is selected, self.selected == 0.
-	local highlightY = nameHeight * (self.selected - 1) - self.scrollY
-	love.graphics.rectangle("fill", 0, highlightY, nameWidth, nameHeight)
-	love.graphics.draw(self.fileCanvas, 0, - self.scrollY)
-
-	love.graphics.setCanvas()
-
-	love.graphics.setColor(1, 1, 1)
-	love.graphics.draw(canvas, bs, bs)
-
-	self.nameBox = {bs, bs, bs + nameWidth, height}
-
-	local nWs = nameWidth + spacing
-	local previewX = bs + nWs
-	local previewY = bs
-	local previewWidth = width - nWs
-	local previewHeight = height
-
-	love.graphics.setColor(0, 0, 0)
-	love.graphics.print("No Preview", previewX, previewY)
---]]
-end
-
 function ListSelector:update(dt)
 	if self.scrollY < self.scrollTo then
 		self.scrollY = self.scrollY + dt * scrollSpeed
@@ -243,60 +233,41 @@ function ListSelector:update(dt)
 
 end
 
+function ListSelector:draw(viewPort)
+	local screenWidth = viewPort.width
+	local screenHeight = viewPort.height
+
+	local size = self.size
+	
+	love.graphics.setCanvas(self.visableCanvas)
+
+	--TODO delete overiding variables
+	self.selected = 1
+	
+	--TODO add scrool offsets
+	love.graphics.draw(self.buttonCanvas, 0, 0)
+	love.graphics.setColor(0.6, 0.6, 0.6)
+	-- When nothing is selected, self.selected == 0.
+	local highlightY = size * (self.selected - 1) - self.scrollY
+	local border = size/8
+	love.graphics.rectangle(
+		"fill",
+		border, highlightY + border,
+		nameWidth, size - 2 * border)
+	
+	love.graphics.setColor(1, 1, 1)
+	love.graphics.draw(self.textCanvas, 0, -self.scrollY)
+
+	love.graphics.setCanvas()
+	
+	
+	local x = self.x + self.horRef * screenWidth  - self.width  * self.horOff
+	local y = self.y + self.verRef * screenHeight - self.height * self.verOff
+
+	love.graphics.draw(self.visableCanvas, x, y)
+	
+	love.graphics.setColor(1, 1, 1)
+end
+
 return ListSelector
 
---[[
-function LoadMenu:createMainCanvas()
-	self.visibleHeight = love.graphics.getHeight() - 2 * bs
-	self.nameBox = {bs, bs, bs + nameWidth, bs + self.visibleHeight}
-
-	local width = love.graphics.getWidth() - 2 * border
-	local height = love.graphics.getHeight() - 2 * border
-
-	local canvas = love.graphics.newCanvas(width, height)
-	love.graphics.setCanvas(canvas)
-
-	love.graphics.clear(0.6, 0.6, 0.6)
-
-	love.graphics.setColor(0.5, 0.5, 0.5)
-	love.graphics.rectangle( "fill",
-		spacing, spacing,
-		nameWidth, height - 2 * spacing)
-
-	love.graphics.setCanvas()
-
-	self.mainCanvas = canvas
-end
-
-function LoadMenu:refreshFiles()
-	local filenames = {}
-	local files = love.filesystem.getDirectoryItems(self.dir)
-	local selected = 0
-	for _, fileName in pairs(files) do
-		local name = string.gsub(fileName, ".txt", "")
-		table.insert(filenames, name)
-		selected = 1
-	end
-
-	self.filenames = filenames
-	self.selected = selected
-
-	-- Canvases must have nonzero dimensions.
-	local canvas = love.graphics.newCanvas(200, nameHeight * (#filenames + 1))
-	love.graphics.setCanvas(canvas)
-
-	love.graphics.setColor(0, 0, 0)
-	for i, name in ipairs(filenames) do
-		love.graphics.print(name, 5, nameHeight * (i - 1) + 5)
-	end
-
-	love.graphics.setCanvas()
-
-	self.fileCanvas = canvas
-	self.scrollMax = canvas:getHeight() - self.visibleHeight
-	if self.scrollMax < 0 then
-		self.scrollMax = 0
-	end
-end
-
---]]

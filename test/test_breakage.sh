310cb45d53850de94ec5b34bff88d852897364ef
Jordan Christiansen <xordspar0@gmail.com>
2023-07-10 17:17:08 -0500

    Make breakage test more robust (#389)
    Since the number of objects before and after the breakage were the same,
    the test would pass even if nothing happened. It was impossible to make
    the test fail. Now, the number of objects actually changes when the shot
    hits the structure, so you can tell something happened.

--- test/test_breakage.sh
+++ test/test_breakage.sh
@@ -25,8 +25,8 @@ wait
 num_objects_before=$(sed -n 1p "$output")
 num_objects_after=$(sed -n 2p "$output")
 
-if ! [ "$num_objects_before" -eq 2 -a "$num_objects_after" -eq 2 ]; then
+if ! [ "$num_objects_before" -eq 2 -a "$num_objects_after" -eq 3 ]; then
 	printf "FAILED: expected 2 objects before breakage (actual: %s) " "$num_objects_before"
-	printf "and 2 objects after (actual: %s)\n" "$num_objects_after"
+	printf "and 3 objects after (actual: %s)\n" "$num_objects_after"
 	return 1
 fi

50c0c314ad6c714d2973548311a0c97ac2f17667
Jordan Christiansen <xordspar0@gmail.com>
2023-07-24 17:55:58 -0500

    Add a test for disconnecting a part from a ship that has a core part (#395)

---
+++ test/test_disconnect_parts_from_active_ship.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+extra_args="$1"
+output="$2"
+
+love src $extra_args --scene test-disconnect-parts-from-active-ship > "$output" <<EOF
+return #world.objects
+world.objects[2]:disconnectPart({1,-1})
+return #world.objects
+quit()
+EOF
+
+num_objects_before=$(sed -n 1p "$output")
+num_objects_after=$(sed -n 2p "$output")
+
+if ! [ "$num_objects_before" -eq 2 -a "$num_objects_after" -eq 3 ]; then
+	printf "FAILED: expected 2 objects before disconnect (actual: %s) " "$num_objects_before"
+	printf "and 3 objects after (actual: %s)\n" "$num_objects_after"
+	return 1
+fi

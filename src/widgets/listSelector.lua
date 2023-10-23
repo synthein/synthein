fe1b7fb0a8633efbf78b2190667e9715ceb14176
randomguyeatspotato <randomguyeatspotato@gmail.com>
2023-10-23 17:11:52 -0500

    Formation cleanup (#421)
    * Did some cleanup of listSelector
    
    * Removed old coments

--- src/widgets/listSelector.lua
+++ src/widgets/listSelector.lua
@@ -3,17 +3,9 @@ local ListSelector = class()
 local scrollSpeed = 150
 
 function ListSelector:__create(size, x, y, width, height, list)
-	self.x = x
-	self.y = y
 	self.width = width
-	self.height = height
-	
+	self.height = height	
 	self.size = size
-	
-	self.horRef = 0
-	self.verRef = 0
-	self.horOff = 0
-	self.verOff = 0
 	
 	self.scrollY = 0
 	self.scrollTo = 0
@@ -25,7 +17,6 @@ function ListSelector:__create(size, x, y, width, height, list)
 	self:setList(list)
 	self.visableCanvas = love.graphics.newCanvas(width, height)
 	self.highlightCanvas = love.graphics.newCanvas(width, size)
-	
 	
 	love.graphics.setCanvas(self.highlightCanvas)
 	love.graphics.setColor(1, 1, 1, 0.75)
@@ -83,68 +74,13 @@ function ListSelector:setList(list)
 		self.scrollMax = 0
 	end--]]
 end
---[[
---TODO rename variables
-function ListSelector:set_reference_points(horRef, verRef, horOff, verOff)
-	if     horRef == "left" then
-		self.horRef = 0
-	elseif horRef == "center" then
-		self.horRef = 0.5
-	elseif horRef == "right" then
-		self.horRef = 1
-	end
-	if     verRef == "top" then
-		self.verRef = 0
-	elseif verRef == "center" then
-		self.verRef = 0.5
-	elseif verRef == "bottom" then
-		self.verRef = 1
-	end
-	if     horOff == "left" then
-		self.horOff = 0
-	elseif horOff == "center" then
-		self.horOff = 0.5
-	elseif horOff == "right" then
-		self.horOff = 1
-	end
-	if     verOff == "top" then
-		self.verOff = 0
-	elseif verOff == "center" then
-		self.verOff = 0.5
-	elseif verOff == "bottom" then
-		self.verOff = 1
-	end
-end --]]
---[[
-function ListSelector:toParentCoords(viewPort)
-	local screenWidth = viewPort.width
-	local screenHeight = viewPort.height
 
-	local x = self.x + self.horRef * screenWidth  - self.width  * self.horOff
-	local y = self.y + self.verRef * screenHeight - self.height * self.verOff
-	
-	return x,y
-end
---
-function ListSelector:toLocalCoords(viewPort, x, y)
-	local screenWidth = viewPort.width
-	local screenHeight = viewPort.height
-
-	x = x - self.x - self.horRef * screenWidth + self.width  * self.horOff
-	y = y - self.y - self.verRef * screenHeight + self.height * self.verOff
-	
-	return x, y
-end
---]]
 function ListSelector:getButtonAt(x, y)
 	local width = self.width
 	local height = self.height
 	local index = 0
 	if 0 < x and x < width and 0 < y and y < height then
 		index = math.floor((y + self.scrollY) / self.size + 1)
-		self.hovering = true
-	else
-		self.hovering = false
 	end
 	if index > #self.list then
 		index = 0
@@ -153,7 +89,21 @@ function ListSelector:getButtonAt(x, y)
 	return index
 end
 
-function ListSelector:keypressed(key)
+function cursorpressed(key, cursor)
+	self.selected = self:getButtonAt(cursor.x, cursor.y)
+	if key == "confirm" then
+		local s = self.selected
+		local len = #self.list
+		if s <= len and s > 0 then
+			return s
+		end
+	end
+end
+
+function cursorreleased(key, cursor)
+end
+
+function ListSelector:pressed(key)
 	local s = self.selected
 	local len = #self.list
 	if len ~= 0 then
@@ -163,16 +113,16 @@ function ListSelector:keypressed(key)
 		elseif key == "down" then
 			s = s + 1
 			if s > len then s = s - len end
-		elseif key == "return" then
-			return s
+		elseif key == "return" or key == "build" then
+			if s <= len and s > 0 then
+				return s
+			end
 		end
 	end
 	self.selected = s
 end
 
-function ListSelector:mousemoved(x, y)
-	local index = self:getButtonAt(x, y)
-	self.selected = index
+function ListSelector:released(key)
 end
 
 function ListSelector:wheelmoved(x, y)
@@ -186,13 +136,6 @@ function ListSelector:wheelmoved(x, y)
 		if self.scrollTo < 0 then
 			self.scrollTo = 0
 		end
-	end
-end
-
-function ListSelector:pressed()
-	local index = self.selected
-	if index ~= 0 and self.hovering then
-		return index
 	end
 end
 
@@ -213,21 +156,12 @@ function ListSelector:update(dt)
 
 end
 
-function ListSelector:cursorHighlight(x, y)
-	local index = self:getButtonAt(x, y)
-	if index ~=0 then
-		self.selected = index
-	end
+function ListSelector:resize()
 end
 
 function ListSelector:draw(viewPort, cursor)
---[[
-	local cx, cy = self:toLocalCoords(viewPort, cursor.x, cursor.y)
-	local index = self:getButtonAt(cx, cy)
-	if index ~=0 then
-		self.selected = index
-	end--]]
-	
+	self.selected = self:getButtonAt(cursor.x, cursor.y)
+
 	local size = self.size
 	local selected = self.selected
 	local scrollY = self.scrollY

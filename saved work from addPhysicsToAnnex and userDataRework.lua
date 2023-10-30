87a17eff7de2ff7ff7613e4516f314a15148b34f
randomguyeatspotato <randomguyeatspotato@gmail.com>
2023-10-30 17:40:56 -0500

    Cleaned out old unused code and took notes (#425)

--- saved work from addPhysicsToAnnex and userDataRework.lua
+++ saved work from addPhysicsToAnnex and userDataRework.lua
@@ -1,172 +1,27 @@
--- annex.lua
-
-Annex = class()
-
-function Annex:__create(team)
-	self.stage = 0
-	self.abort = false
-	self.team = team
+-- Discuss
 
 
-	local structureHandle = {}
-	function structureHandle.Abort()
-		self.abort
-end
+-- General Concepts:
+	-- Check annex permistions
+		-- Other structure approval
+			-- This is only relavent in hacker prevention not relavent in single instance
+			-- Server might be able to verify this.
+		-- Approval can be gained before things are flagged
+			-- Builders can keep a list of valid sources and destinations
+	-- Add parts to receving structure with the relavent vectors
+		-- What parts are valid to connect
+		-- Break off parts that don't fit
+	-- Flag recieving structure to be unrecemptive to build commands
+	-- Flag giving structure to be unrecemptive to build commands
+		-- or remove parts
+	-- On recieving structure update add new parts clear flag
+	-- Moving into positon during Annex or before Annex
+		-- During
+			-- Requires the annex to exist for a period of time
+			-- Would interact with the physics
+				-- 180 rotational lock problem.
+			-- More difficult to know which parts have valid connections.
+			-- Needs to properly abort if it can't move into place.
+		-- Before annex requires more advanced user interface.
+			-- Some setups May reintroduce the rotational lock problem
 
-function Annex:abort()
-	self.abort = true
-end
-
-function Annex:getRequest()
-	local stage = self.stage
-	if     stage == 0 then
-		-- check for part and connectableSides
-	elseif stage == 1 then
-	elseif stage == 2 then
-	elseif stage == 3 then
-	elseif stage == 4 then
-	elseif stage ==
-
-
-end
-
-return Annex
-
--- structure.lua
-
-function Structure:update(dt)
-print(self.isDestroyed)
-	local partsInfo = self:command(dt)
-	self.shield:update(dt)
-
-	if self.annexBundle then
-		if self.corePart then
-			if self.annexBundle.parts then
-				self:annex(self.annexBundle.parts, unpack(self.annexBundle.inputs))
-				self.annexBundle = nil
-			end
-			self.annexBundle.goalA = {self.body:getPosition()}
-			if self.annexBundle.goalS then
-				local a = self.annexBundle.goalA
-				local s = self.annexBundle.goalS
-				local dx = s[1] - a[1]
-				local dy = s[2] - a[2]
-				self.body:applyForce(dx, dy)
-			end
-		else
-			self.annexBundle.goalS = {self.body:getPosition()}
-			if self.annexBundle.goalA then
-				local a = self.annexBundle.goalA
-				local s = self.annexBundle.goalS
-				local dx = a[1] - s[1]
-				local dy = a[2] - s[2]
-				self.body:applyForce(dx, dy)
-
-				if false then
-					self.annexBundle.parts = self.gridTable:loop()
-					for _, part in ipairs(self.annexBundle.parts) do
-						self:removePart(part)
-					end
-					self.annexBundle = nil
-				end
-			end
-		end
-	end
-end
-
--- annexRequest.lua
-
- local Annex = class()
-
-function Annex:__create(
-		bodyUserData, partLocation,
-		annexeeBodyUserData, nnexeePartLocation)
-
-	self.denied = false
-	self.replied = false
-	self.partLocation = partLocation
-	self.annexeePartLocation = annexeePartLocation
-
-
-
-end
-
-function Annex:deny()
-	self.denied = true
-end
-
-function Annex:isDenied()
-	return self.denied
-end
-
-function Annex:hasReplied()
-	return self.replied
-end
-
-function Annex:getLocations()
-	return partLocation, annexeePartLocation
-end
-
-function Annex:getRequest()
-	local function f(structure)
-		self.replied = true
-		self.annexee = structure
-		return true
-	 end
-	 return f
-end
-
-function Annex:getAtempt()
-	local function f(structure)
-		if self.annexee then return false end
-		structure:annex(
-			self.annexee, self.annexeePart,
-			self.annexeePartSide,
-			self.structurePart, self.structurePartSide)
-		return true
-end
-
-return Annex
-
--- structure.lua
-
- function Structure:update(dt)
-	local userData = self.body:getUserData()
-	local buildRequest = userData.buildRequest
-	if buildRequest then
-		local team = userData.getTeam()
-		if buildRequest.type == 1 then
-			if team == buildRequest.team or team == 0 then
-				userData.buildRequest = nil
-				if buildRequest.disconnect then
-					self:disconnectPart(buildRequest.aPart, false)
-				else
-					self:annex(
-						buildRequest.b, buildRequest.bPart, buildRequest.bSide,
-						buildRequest.aPart, buildRequest.aSide)
-				end
-			end
-		elseif buildRequest.type == 2 then
-			if team == 0 then
-				userData.buildRequest = nil
-				buildRequest.b = self
-			end
-		end
-	end
-
-	local partsInfo = self:command(dt)
-	self.shield:update(dt)
-end
-
--- beginning of Selection:pressed()
-
-		-- build request disconnect
-		if not structure.buildRequest then
-			local buildRequest = {}
-			buildRequest.type = 1
-			buildRequest.disconnect = true
-			buildRequest.team = self.team
-			buildRequest.aPart = {roundedX, roundedY}
-
-			structure.buildRequest = buildRequest
-		end

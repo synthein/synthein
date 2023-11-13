ff9797b701ea8c778d33b120bf053412a33b97b8
Jordan Christiansen <xordspar0@gmail.com>
2023-11-13 17:05:30 -0600

    Replace lume.clamp with a Rust function
    Rust comes with a clamp function anyway, so we may as well use it.

---
+++ src/mathext.rs
@@ -0,0 +1,15 @@
+use mlua::prelude::{LuaTable};
+use mlua::{Lua, Result};
+
+pub fn lua_module(lua: &Lua) -> Result<LuaTable> {
+    let exports = lua.create_table()?;
+
+    exports.set(
+        "clamp",
+        lua.create_function(|_lua: &Lua, (input, min, max): (f64, f64, f64)| {
+            Ok(input.clamp(min, max))
+        })?,
+    )?;
+
+    Ok(exports)
+}

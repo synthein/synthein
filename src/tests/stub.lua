6f02133a30144a8bc082956175a1df6a83771bfc
Jordan Christiansen <xordspar0@gmail.com>
2024-04-05 16:38:05 -0500

    Fix crash in headless mode (#457)
    The game was crashing in headless mode because of a comparison with a
    stub in canvasUtils. canvasUtils.isWithin() gets the dimensions of a
    canvas and then does comparisons on it. Since canvases are stubbed,
    getDimensions returns a stub, which was not comparable until now.
    
    Also, add the field `stub = true` to stubs to make it easier to notice
    them when debugging.
    
    I'll leave a marker.
    Lost ones will see it and say:
    Ah, this is a stub.

--- src/tests/stub.lua
+++ src/tests/stub.lua
@@ -1,5 +1,5 @@
 return function()
-  local stub = {}
+  local stub = {stub = true}
   return setmetatable(stub, {
     __index = function() return stub end,
     __call = function() return stub, stub end,
@@ -7,5 +7,6 @@ return function()
     __sub = function() return 1 end,
     __mul = function() return 1 end,
     __div = function() return 1 end,
+    __lt = function() return true end,
   })
 end

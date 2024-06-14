synthein/synthein

commit 52c69cf4f813daaab432f1045740a0b91575f619

Add smooth camera movement (#459)
When connecting a part to a structure, the camera moves to that structure
while doing the build action. Now that camera movement is linearly
interpolated.

I added a new Animation class. It could potentially support other
animation functions.

It's aware of x, y, and angle. It could be renamed cameraPan.lua, or
maybe in the future it will support multiple animations, one of which
is a pan. We'll see.

src/camera.lua
@@ -1,3 +1,4 @@
+local Animation = require("animation")
 local Hud = require("hud")
 local PhysicsReferences = require("world/physicsReferences")
 local Settings = require("settings")
@@ -25,6 +26,15 @@ function Camera.create()
 	self.hud = Hud()
 	
 	return self
+end
+
+function Camera:setTarget(target)
+	local duration = 1
+	local x, y = self.body:getPosition()
+	local angle = self.body:getAngle()
+
+	self.body = target
+	self.pan = Animation({x, y, angle}, {target:getX(), target:getY(), target:getAngle()}, duration, "linear")
 end
 
 function Camera:getWorldCoords(cursorX, cursorY)
@@ -287,6 +297,32 @@ function Camera:drawWorldObjects(player, debugmode)
 	player.shieldPoints = player.camera:testPoints(testPointFunctions)
 end
 
+local function shortestPath(angle, newAngle)
+	local angleDiff = newAngle - angle
+	if angleDiff > math.pi then
+		angleDiff = angleDiff - 2*math.pi
+	end
+	return angle + angleDiff
+end
+
+function Camera:update(player, dt)
+	if self.body then
+		local newX, newY = self.body:getPosition()
+		local newAngle = shortestPath(
+			self.angle,
+			player.isCameraAngleFixed and 0 or body:getAngle() % (2*math.pi)
+		)
+
+		if self.pan then
+			self.x, self.y, self.angle = self.pan:step(dt, {newX, newY, newAngle})
+
+			if self.pan:isDone() then
+				self.pan = nil
+			end
+		end
+	end
+end
+
 function Camera:drawPlayer(player, debugmode)
 	local compassAngle = 0
 
@@ -297,9 +333,6 @@ function Camera:drawPlayer(player, debugmode)
 			self.body = nil
 			self.gameOver = true
 		else
-			self.x, self.y = body:getPosition()
-			self.angle = player.isCameraAngleFixed and 0 or body:getAngle()
-
 			local point = {0,0}
 
 			local leader = (player.ship.corePart or {}).leader

src/animation.lua
@@ -1,0 +1,33 @@
+local Animation = class()
+
+local funcs = {
+  linear = function(s, d, r) return (d-s)*r + s end
+}
+
+function Animation:__create(start, dest, duration, func)
+  self.start = start
+  self.dest = dest
+  self.elapsed = 0
+  self.duration = duration
+  self.func = funcs[func]
+end
+
+function Animation:step(dt, dest)
+  if dest then
+    self.dest = dest
+  end
+
+  self.elapsed = self.elapsed + dt
+
+  local x = self.func(self.start[1], self.dest[1], self.elapsed / self.duration)
+  local y = self.func(self.start[2], self.dest[2], self.elapsed / self.duration)
+  local a = self.func(self.start[3], self.dest[3], self.elapsed / self.duration)
+
+  return x, y, a
+end
+
+function Animation:isDone()
+  return self.elapsed >= self.duration
+end
+
+return Animation

src/gamestates/inGame.lua
@@ -295,6 +295,7 @@ function InGame.update(dt)
 		player.menuOpen = menuOpen
 
 		player:handleInput()
+		player:update(dt)
 	end
 
 	if closeMenu then

src/player.lua
@@ -19,6 +19,14 @@ function Player.create(world, controls, ship, camera)
 		self.camera.body = ship.body
 		self.selected = Selection.create(world, self.ship.corePart:getTeam())
 
+		self.selected:whenBuildingOnStructure(function()
+			self.camera:setTarget(self.selected.structure.body)
+		end)
+
+		self.selected:whenDoneBuildingOnStructure(function()
+			self.camera:setTarget(self.ship.body)
+		end)
+
 		local corePart = ship.corePart
 		if corePart then
 			self.camera.hud:setCommand(corePart:getCommand())
@@ -212,15 +220,11 @@ function Player:buttonreleased(source, button)
 	end
 end
 
-function Player:draw(debugmode)
-	if self.ship then
-		if self.selected:isBuildingOnStructure() then
-			self.camera.body = self.selected.structure.body
-		else
-			self.camera.body = self.ship.body
-		end
-	end
+function Player:update(dt)
+	self.camera:update(self, dt)
+end
 
+function Player:draw(debugmode)
 	self.camera:drawPlayer(self, debugmode)
 end
 

src/selection.lua
@@ -19,6 +19,9 @@ function Selection.create(world, team)
 
 	self.assign = nil
 
+	self.buildingOnStructureListeners = {}
+	self.doneBuildingOnStructureListeners = {}
+
 	return self
 end
 
@@ -40,6 +43,7 @@ function Selection:cursorpressed(cursor, control)
 							self.part = nil
 							self.build = nil
 						end
+						self:signalBuildingOnStructure()
 					end
 				end
 			elseif control.ship == "destroy" then
@@ -96,6 +100,7 @@ function Selection:cursorreleased(cursor, control)
 					build:setSide(partSide)
 					if build.mode == 5 then
 						self.build = nil
+						self:signalDoneBuildingOnStructure()
 					end
 				else
 					self.build = nil
@@ -122,108 +127,28 @@ function Selection:cursorreleased(cursor, control)
 	end
 end
 
-function Selection:pressed(cursorX, cursorY, order)
-	local structure = self.world:getObject(cursorX, cursorY)
-	local part
-	if structure then part = structure:findPart(cursorX, cursorY) end
-	if structure and structure.type == "structure" and part then
-		local build = self.build
-		local team = structure.body:getUserData().team
-		if build then
-			if order == "build" then
-				if build.mode == 3 then
-					if team == 0 or team == self.team then
-						self.structure = structure
-						self.part = part
-						if build:setStructure(structure, part) then
-							self.structure = nil
-							self.part = nil
-							self.build = nil
-						end
-					end
-				end
-			elseif order == "destroy" then
-				self.structure = nil
-				self.part = nil
-				self.build = nil
-			end
-		elseif self.assign then
-			self.assign.leader = structure
-			self.assign = nil
-		else
-			if order == "build" then
-				if team ~= 0 then
-					local corePart = structure.corePart
-					if corePart == part then
-						if team == self.team and corePart.modules.drone then
-							self.structure = structure
-							self.part = part
-						end
-					end
-				else
-					self.build = Building.create()
-					self.build:setAnnexee(structure, part)
-					self.structure = structure
-					self.part = part
-				end
-			elseif order == "destroy" then
-				local corePart = structure.corePart
-				if team == 0 or (team == self.team and part ~= corePart) then
-					structure:disconnectPart(part.location)
-				end
-			end
-		end
-
-	else
-		if order == "destroy" then
-			self.structure = nil
-			self.part = nil
-			self.build = nil
-		end
-	end
-end
-
-function Selection:released(cursorX, cursorY)
-	local structure = self.structure
-	local part = self.part
-	if structure and part then
-		local l = part.location
-		local partSide, withinPart = StructureMath.getPartSide(structure, l, cursorX, cursorY)
-		local build = self.build
-		if not withinPart then
-			if build then
-				if structure:testEdge({l[1], l[2], partSide}) then
-					build:setSide(partSide)
-					if build.mode == 5 then
-						self.build = nil
-					end
-				else
-					self.build = nil
-				end
-			else
-				local body = structure.body
-				local x, y = body:getWorldPoints(l[1], l[2])
-				local strength = part:getMenu()
-				local newAngle = vector.angle(cursorX - x, cursorY - y)
-				local index = CircleMenu.angleToIndex(newAngle, #strength)
-				local option = self.part:runMenu(index, body)
-				if option == "assign" then
-					self.assign = self.part
-				end
-			end
-			self.structure = nil
-			self.partSide = nil
-		else
-			if not build then
-				self.structure = nil
-				self.partSide = nil
-			end
-		end
-	end
-end
-
 function Selection:isBuildingOnStructure()
 	return self.build and self.build.structure
+end
+
+function Selection:whenBuildingOnStructure(func)
+	table.insert(self.buildingOnStructureListeners, func)
+end
+
+function Selection:signalBuildingOnStructure()
+	for _, func in ipairs(self.buildingOnStructureListeners) do
+		func()
+	end
+end
+
+function Selection:whenDoneBuildingOnStructure(func)
+	table.insert(self.doneBuildingOnStructureListeners, func)
+end
+
+function Selection:signalDoneBuildingOnStructure()
+	for _, func in ipairs(self.doneBuildingOnStructureListeners) do
+		func()
+	end
 end
 
 return Selection

commit 78ccdd8815e474b7925e51be437cacaa9451639a

Fix camera position updating (#467)
A bug was introduced late in 52c69cf that caused camera movement to be
frozen when an animation was not in progress. Now the camera always
follows its set target as it should.

src/camera.lua
@@ -347,6 +347,8 @@ function Camera:update(player, dt)
 			if self.pan:isDone() then
 				self.pan = nil
 			end
+		else
+			self.x, self.y, self.angle = newX, newY, newAngle
 		end
 	end
 end
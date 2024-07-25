b617cc3ad3b6626c34034319e3d24b154171ea79
Jordan Christiansen <xordspar0@gmail.com>
2024-07-25 05:49:08 +0000

    Make camera pan follow an ease-out curve
    This is a quadratic bezier curve that starts out fast and ends slow. The
    curve could be described as quadratic_bezier(src, dest, dest), or
    
    	A = lerp(src, dest)
    	B = dest
    	out = lerp(A, B)
    
    I started out with this double-lerp formula and then simplified it
    algebraicly.

--- src/animation.lua
+++ src/animation.lua
@@ -1,7 +1,8 @@
 local Animation = class()
 
 local funcs = {
-  linear = function(s, d, r) return (d-s)*r + s end
+  linear  = function(s, d, t) return (d-s)*t + s end,
+  easeout = function(s, d, t) return (s-d)*t*t + 2*(d-s)*t + s end,
 }
 
 function Animation:__create(start, dest, duration, func)

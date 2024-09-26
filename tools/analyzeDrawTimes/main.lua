6be6ffe2ce90e261e164919fbc138c18b5dd7230
Jordan Christiansen <xordspar0@gmail.com>
2024-09-26 17:16:40 -0500

    tools/analyzeDrawTimes: Add axis labels (#477)

--- tools/analyzeDrawTimes/main.lua
+++ tools/analyzeDrawTimes/main.lua
@@ -1,6 +1,6 @@
 -- Settings
 local logfile = "draw-times.log"
-local margin = 30
+local margin = 50
 local nBuckets = 20
 
 -- State
@@ -38,6 +38,9 @@ function love.draw()
   local chartWidth = love.graphics.getWidth() - margin
   local chartHeight = love.graphics.getHeight() - margin
 
+  love.graphics.print("Number of frames", 10, love.graphics.getHeight()/2, -math.pi/2)
+  love.graphics.print("Duration of frame (ms)", love.graphics.getWidth()/2, chartHeight + 30)
+
   local buckets = {}
   for _, v in ipairs(times) do
     local bucketIndex = 1 + math.floor((v - min) / (max - min) * (nBuckets-1))
@@ -61,6 +64,13 @@ function love.draw()
     local label = tostring(tonumber(v))
     local textWidth = font:getWidth(label)
     love.graphics.print(label, x + (w - textWidth)/2, chartHeight-h-20)
+
+    if i % 2 == 0 then
+      label = string.format("%0.1f", min+i*(max-min)/nBuckets*1000)
+      textWidth = font:getWidth(label)
+      love.graphics.line(x + w/2, chartHeight, x + w/2, chartHeight+10)
+      love.graphics.print(label, x + (w - textWidth)/2, chartHeight+10)
+    end
   end
 end
 

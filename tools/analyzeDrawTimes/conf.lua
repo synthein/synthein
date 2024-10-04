f3dc755a5ddbd664c9854f83968a84cc2d4e81b3
Jordan Christiansen <xordspar0@gmail.com>
2024-10-03 19:40:00 -0500

    tools/analyzeDrawTimes: Simplify and clarify x axis ticks (#478)
    * tools/analyzeDrawTimes: Simplify and clarify x axis ticks
    
    - Always set 0 as the minimum of the chart. This removes the need to
      search for the minimum value in the input and scale to the minimum.
    - Draw the tick marks between bars. A bar is a range of values. The
      boundaries are more interesting than the middle.
    
    * tools/analyzeDrawTimes: Optimize for smaller screens

--- tools/analyzeDrawTimes/conf.lua
+++ tools/analyzeDrawTimes/conf.lua
@@ -5,8 +5,8 @@ function love.conf(t)
 
 	t.window.title = "Synthein - Draw Times"
 	t.window.icon = "tools.png"
-	t.window.width = 800
-	t.window.height = 600
+	t.window.width = 640
+	t.window.height = 480
 	t.window.borderless = false
 	t.window.resizable = false
 	t.window.fullscreen = false
